c6c5d576966954d9c6468f0e5739aaa24a87c19c
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-11-06 22:42:44 +0100

    Update CommandMenu.tsx
    Co-authored-by: Håvard Gjøby Thom <34199185+havardthom@users.noreply.github.com>

--- frontend/src/components/CommandMenu.tsx
+++ frontend/src/components/CommandMenu.tsx
@@ -7,7 +7,7 @@ import {
   CommandList,
 } from "@/components/ui/command";
 import { fetchCategories } from "@/lib/data";
-import { Category } from "@/lib/types"; // Removed unused Script import
+import { Category } from "@/lib/types";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
