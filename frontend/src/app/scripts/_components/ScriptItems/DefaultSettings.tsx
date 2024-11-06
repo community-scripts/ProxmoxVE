405175295a7b521478930007d93dcc9909c1bbec
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-11-06 22:43:02 +0100

    Update DefaultSettings.tsx
    Co-authored-by: Håvard Gjøby Thom <34199185+havardthom@users.noreply.github.com>

--- frontend/src/app/scripts/_components/ScriptItems/DefaultSettings.tsx
+++ frontend/src/app/scripts/_components/ScriptItems/DefaultSettings.tsx
@@ -8,7 +8,7 @@ export default function DefaultSettings({ item }: { item: Script }) {
   const defaultSettingsAvailable =
     defaultSettings?.resources.cpu ||
     defaultSettings?.resources.ram !== null ||
-    defaultSettings?.resources.hdd !== null;
+    defaultSettings?.resources.hdd;
 
   const defaultAlpineSettings = item.install_methods.find(
     (method) => method.type === "alpine",
