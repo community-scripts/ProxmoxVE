1df1667a07482edb46c7c35e62a15762db4a3233
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-11-07 21:53:38 +0100

    Apply suggestions from code review
    Co-authored-by: Håvard Gjøby Thom <34199185+havardthom@users.noreply.github.com>

--- install/homarr-install.sh
+++ install/homarr-install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2024 tteck
-# Author: MickLesk (Canbiz)
+# Author: tteck (tteckster)
+# Co-Author: MickLesk (Canbiz)
 # License: MIT
 # https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/ajnart/homarr
