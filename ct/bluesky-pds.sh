205c9c9c6eb3c70fc1b6a7d4ee194314b4a2e1ce
Sujeethan Vigneswaran <sujeethan.vigneswaran@gmail.com>
2024-11-13 23:04:31 -0500

    BlueSky PDS container creation script

---
+++ ct/bluesky-pds.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2024 tteck
+# Author: itssujee
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+start
+build_container
+description
+
+function header_info {
+clear
+cat <<"EOF"
+    ____  __          _____ __            ____  ____  _____
+   / __ )/ /_  _____ / ___// /____  __   / __ \/ __ \/ ___/
+  / __  / / / / / _ \\__ \/ //_/ / / /  / /_/ / / / /\__ \ 
+ / /_/ / / /_/ /  __/__/ / ,< / /_/ /  / ____/ /_/ /___/ / 
+/_____/_/\__,_/\___/____/_/|_|\__, /  /_/   /_____//____/  
+                             /____/                        
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="BlueSky PDS"
+var_disk="20"
+var_cpu="1"
+var_ram="2048"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} should be reachable by going to the following URL. http://${IP} \n" 
---
+++ install/bluesky-pds-install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2024 tteck
+# Author: itssujee
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
+msg_ok "Installed Dependencies"
+
+msg_info "Installing BlueSky PDS"
+$STD sh <(curl -sSL https://raw.githubusercontent.com/bluesky-social/pds/main/installer.sh)
+systemctl stop pds
+sudo pdsadmin update
+systemctl start pds
+msg_ok "Installed BlueSky PDS"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
