ffdc626c71f78f77b935ce8fcc310dec0182ebf7
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-11-16 20:47:36 +0100

    Update komga.sh

--- ct/komga.sh
+++ ct/komga.sh
@@ -21,7 +21,7 @@ check_container_storage
 check_container_resources
 echo -e "Loading..."
 APP="Komga"
-var_disk="2"
+var_disk="4"
 var_cpu="1"
 var_ram="2048"
 var_os="debian"
@@ -56,7 +56,9 @@ function default_settings() {
 
 function update_script() {
 header_info
-if [[ ! -f /opt/komga ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+check_container_storage
+check_container_resources
+if [[ ! -f /opt/komga/komga*.jar ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
 msg_info "Updating ${APP}"
 RELEASE=$(curl -s https://api.github.com/repos/gotson/komga/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
 if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
@@ -64,18 +66,19 @@ if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_v
   systemctl stop komga
   msg_ok "Stopped ${APP}"
 
-  msg_info "Updating ${APP} to $RELEASE"
-  wget -q "https://github.com/gotson/komga/releases/download/v$RELEASE/komga-${RELEASE}.jar"
+  msg_info "Updating ${APP} to ${RELEASE}"
+  rm -rf /opt/komga/komga*.jar
+  wget -q "https://github.com/gotson/komga/releases/download/v${RELEASE}/komga-${RELEASE}.jar"
   mv -f komga-${RELEASE}.jar /opt/komga/komga.jar
   echo "${RELEASE}" >/opt/${APP}_version.txt
-  msg_ok "Updated ${APP} to $RELEASE"
+  msg_ok "Updated ${APP} to ${RELEASE}"
 
   msg_info "Starting ${APP}"
   systemctl start komga
   msg_ok "Started ${APP}"
   msg_ok "Updated Successfully"
 else
-  msg_ok "No update required.  ${APP} ia already at ${RELEASE}."  
+  msg_ok "No update required. ${APP} is already at v${RELEASE}."  
 fi
 exit
 }
