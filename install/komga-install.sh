0fc86942f1388bd7227b5654c2a3baf6d5e4dea5
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-11-16 20:41:02 +0100

    Harmonize Script

--- install/komga-install.sh
+++ install/komga-install.sh
@@ -14,12 +14,15 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y curl mc sudo openjdk-17-jre
+$STD apt-get install -y \
+curl \
+mc \
+sudo \
+openjdk-17-jre
 msg_ok "Installed Dependencies"
 
-RELEASE=$(curl -s https://api.github.com/repos/gotson/komga/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
-
 msg_info "Installing Komga"
+RELEASE=$(curl -s https://api.github.com/repos/gotson/komga/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
 wget -q https://github.com/gotson/komga/releases/download/${RELEASE}/komga-${RELEASE}.jar
 mkdir -p /opt/komga
 mv -f komga-${RELEASE}.jar /opt/komga/komga.jar
@@ -42,7 +45,6 @@ Restart=on-failure
 [Install]
 WantedBy=multi-user.target
 EOF
-systemctl -q daemon-reload
 systemctl enable --now -q komga
 msg_ok "Created Service"
 
