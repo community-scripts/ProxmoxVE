9b9337690e3713ace85b6d68c4ae168cb6fbd80f
DarmokNoob <Colin.oppenheim@gmail.com>
2024-11-24 19:48:49 -0800

    Update install/unifi-install.sh
    Co-authored-by: Håvard Gjøby Thom <34199185+havardthom@users.noreply.github.com>

--- install/unifi-install.sh
+++ install/unifi-install.sh
@@ -28,7 +28,9 @@ $STD apt-get update
 $STD apt-get install -y temurin-17-jre
 msg_ok "Installed Eclipse Temurin JRE"
 
-if [ "$MONGO_VERSION" = "4.2" ]; then
+if grep -q 'avx' /proc/cpuinfo; then
+  msg_ok "No AVX Support Detected"
+  msg_info "Installing MongoDB 4.2"
   # Check for libssl1.1 and install if missing
   if ! dpkg -l | grep -q "libssl1.1"; then
     msg_info "libssl1.1 not found. Installing..."
