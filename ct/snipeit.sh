d1933ff5b8c84930bd2ac784f6e514982ef9b60a
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2024-11-27 14:47:10 +0100

    Fixed Copyright Header

--- ct/snipeit.sh
+++ ct/snipeit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
-# Copyright (c) 2021-2024 tteck
-# Author: tteck (tteckster)
+#Copyright (c) 2021-2024 community-scripts ORG
+# Author: Michel Roegl-Brunner (michelroegl-brunner)
 # License: MIT
 # https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 function header_info {
