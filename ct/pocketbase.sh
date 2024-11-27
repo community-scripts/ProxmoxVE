16a9693a49549e8272e7d4dd090820413d34dca4
Dominik Siebel <dominik.siebel@trivago.com>
2024-11-28 00:05:41 +0100

    check for availability of pocketbase binary before running update

--- ct/pocketbase.sh
+++ ct/pocketbase.sh
@@ -56,7 +56,7 @@ function update_script() {
   header_info
   check_container_storage
   check_container_resources
-  if [[ ! -f /etc/systemd/system/pocketbase.service ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+  if [[ ! -f /etc/systemd/system/pocketbase.service || ! -x /opt/pocketbase/pocketbase ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
 	/opt/pocketbase/pocketbase update
 	msg_info "Restarting ${APP}"
 	systemctl restart pocketbase.service
