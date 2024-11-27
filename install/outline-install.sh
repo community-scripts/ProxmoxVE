a57d8ac14ec615fa2ad71b0446df599c13ac5986
Gerhard Burger <burger.ga@gmail.com>
2024-11-27 14:45:24 +0100

    Use node version directly in url

--- install/outline-install.sh
+++ install/outline-install.sh
@@ -25,10 +25,9 @@ $STD apt-get install -y \
 msg_ok "Installed Dependencies"
 
 msg_info "Setting up Node.js Repository"
-NODE_MAJOR=20
 mkdir -p /etc/apt/keyrings
 wget -qO- https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_MAJOR}.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
 msg_ok "Set up Node.js Repository"
 
 msg_info "Setting up Redis Repository"
