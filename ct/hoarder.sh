60d56149cacc23ce18b2fa1d88a9a324e484ba4f
vhsdream <67816022+vhsdream@users.noreply.github.com>
2024-11-28 16:38:55 -0500

    Update hoarder.sh

--- ct/hoarder.sh
+++ ct/hoarder.sh
@@ -20,7 +20,7 @@ header_info
 echo -e "Loading..."
 APP="Hoarder"
 var_disk="8"
-var_cpu="4"
+var_cpu="2"
 var_ram="4096"
 var_os="debian"
 var_version="12"
@@ -58,39 +58,33 @@ check_container_storage
 check_container_resources
 if [[ ! -d /opt/hoarder ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
 RELEASE=$(curl -s https://api.github.com/repos/hoarder-app/hoarder/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
-PREV_VERSION=$(cat /opt/${APP}_version.txt)
-if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "${PREV_VERSION}" ]]; then
+PREV_RELEASE=$(cat /opt/${APP}_version.txt)
+if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "${PREV_RELEASE}" ]]; then
   msg_info "Stopping Services"
   systemctl stop hoarder-web hoarder-workers hoarder-browser
   msg_ok "Stopped Services"
-
   msg_info "Updating ${APP} to v${RELEASE}"
   cd /opt
-  mv /opt/hoarder /opt/hoarder_bak
+  mv /opt/hoarder/.env /opt/.env
+  rm -rf /opt/hoarder
   wget -q "https://github.com/hoarder-app/hoarder/archive/refs/tags/v${RELEASE}.zip"
   unzip -q v${RELEASE}.zip
   mv hoarder-${RELEASE} /opt/hoarder
   cd /opt/hoarder/apps/web
   pnpm install --frozen-lockfile &>/dev/null
-  cd /opt/hoarder/apps/workers
-  pnpm install --frozen-lockfile >/dev/null 2>&1
-  cd /opt/hoarder/apps/web
-  export NEXT_TELEMETRY_DISABLED=1
   pnpm exec next build --experimental-build-mode compile >/dev/null 2>&1
   cp -r /opt/hoarder/apps/web/.next/standalone/apps/web/server.js /opt/hoarder/apps/web
-  export DATA_DIR=/opt/hoarder_data
-  cd /opt/hoarder/packages/db
-  pnpm migrate >/dev/null 2>&1
-  echo "${RELEASE}" >/opt/${APP}_version.txt
-  cp /opt/hoarder_bak/.env /opt/hoarder/.env
-  sed -i "s/SERVER_VERSION=${PREV_VERSION}/SERVER_VERSION=${RELEASE}/" /opt/hoarder/.env
+  cd /opt/hoarder/apps/workers
+  pnpm install --frozen-lockfile >/dev/null 2>&1
+  mv /opt/.env /opt/hoarder/.env
+  sed -i "s/SERVER_VERSION=${PREV_RELEASE}/SERVER_VERSION=${RELEASE}/" /opt/hoarder/.env
   msg_ok "Updated ${APP} to ${RELEASE}"
-  msg_info "Starting ${APP} Services"
+  msg_info "Starting Services"
   systemctl start hoarder-browser hoarder-workers hoarder-web
-  msg_ok "Started ${APP}"
+  msg_ok "Started Services"
   msg_info "Cleaning up"
   rm -R /opt/v${RELEASE}.zip
-  rm -rf /opt/hoarder_bak
+  echo "${RELEASE}" >/opt/${APP}_version.txt
   msg_ok "Cleaned"
   msg_ok "Updated Successfully"
 else
