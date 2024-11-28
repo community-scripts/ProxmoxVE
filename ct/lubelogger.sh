f5c11115bec561fc351c7ef920b37e1d47b1961c
Kristo Copani <kristocopani@gmail.com>
2024-11-28 23:34:27 +0200

    initial commit

---
+++ ct/lubelogger.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/kristocopani/ProxmoxVE/build/misc/build.func)
+# Copyright (c) 2021-2024 community-scripts ORG
+# Author: kristocopani
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+    __          __         __                               
+   / /   __  __/ /_  ___  / /   ____  ____ _____ ____  _____
+  / /   / / / / __ \/ _ \/ /   / __ \/ __ `/ __ `/ _ \/ ___/
+ / /___/ /_/ / /_/ /  __/ /___/ /_/ / /_/ / /_/ /  __/ /    
+/_____/\__,_/_.___/\___/_____/\____/\__, /\__, /\___/_/     
+                                   /____//____/             
+
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="LubeLogger"
+var_disk="2"
+var_cpu="1"
+var_ram="512"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+function update_script() {
+header_info
+check_container_storage
+check_container_resources
+if [[ ! -d /var ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+msg_info "Updating $APP LXC"
+apt-get update &>/dev/null
+apt-get -y upgrade &>/dev/null
+msg_ok "Updated $APP LXC"
+exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} should be reachable by going to the following URL.
+         ${BL}http://${IP}${CL} \n"
---
+++ install/lubelogger-install.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 community-scripts ORG
+# Author: kristocopani
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+    curl \
+    wget \
+    mc \
+    zip \
+    jq
+msg_ok "Installed Dependencies"
+
+
+msg_info "Installing LubeLogger"
+cd /opt
+RELEASE=$(curl -s https://api.github.com/repos/hargata/lubelog/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+RELEASE_TRIMMED=$(echo "${RELEASE}" | tr -d ".")
+wget -q https://github.com/hargata/lubelog/releases/download/v${RELEASE}/LubeLogger_v${RELEASE_TRIMMED}_linux_x64.zip
+mkdir /opt/lubelogger
+$STD unzip -qq LubeLogger_v141_linux_x64.zip -d lubelogger
+chmod 700 /opt/lubelogger/CarCareTracker
+cp /opt/lubelogger/appsettings.json /opt/lubelogger/appsettings_bak.json
+jq '.Kestrel = {"Endpoints": {"Http": {"Url": "http://0.0.0.0:5000"}}}' /opt/lubelogger/appsettings_bak.json > /opt/lubelogger/appsettings.json
+echo "${RELEASE}" >"/opt/${APPLICATION}_version.txt"
+msg_ok "Installed LubeLogger"
+
+msg_info "Creating Service"
+service_path="/etc/systemd/system/lubelogger.service"
+echo "[Unit]
+Description=LubeLogger Daemon
+After=network.target
+
+[Service]
+User=root
+
+Type=simple
+WorkingDirectory=/opt/lubelogger
+ExecStart=/opt/lubelogger/CarCareTracker
+TimeoutStopSec=20
+KillMode=process
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target" >$service_path
+
+systemctl enable --now -q lubelogger.service
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm -rf /opt/lubelogger/appsettings_bak.json
+rm -rf /opt/LubeLogger_v${RELEASE_TRIMMED}_linux_x64.zip
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
