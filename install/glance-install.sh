5971bc76f3b35aa99715052bb373687ef94225ad
Kristo Copani <kristocopani@gmail.com>
2024-11-29 15:21:58 +0200

    small fixes

--- install/glance-install.sh
+++ install/glance-install.sh
@@ -16,7 +16,7 @@ update_os
 msg_info "Installing Dependencies"
 $STD apt-get install -y \
     curl \
-    wget \
+    sudo \
     mc 
 msg_ok "Installed Dependencies"
 
@@ -28,6 +28,7 @@ wget -q https://github.com/glanceapp/glance/releases/download/v${RELEASE}/glance
 mkdir /opt/glance
 tar -xzf glance-linux-amd64.tar.gz -C /opt/glance
 cat <<EOF >/opt/glance/glance.yml
+#Documentation link https://github.com/glanceapp/glance/blob/main/docs/configuration.md
 pages:
   - name: Startpage
     width: slim
@@ -39,29 +40,6 @@ pages:
           - type: search
             autofocus: true
 
-          - type: monitor
-            cache: 1m
-            title: Services
-            sites:
-              - title: Jellyfin
-                url: https://yourdomain.com/
-                icon: si:jellyfin
-              - title: Gitea
-                url: https://yourdomain.com/
-                icon: si:gitea
-              - title: qBittorrent # only for Linux ISOs, of course
-                url: https://yourdomain.com/
-                icon: si:qbittorrent
-              - title: Immich
-                url: https://yourdomain.com/
-                icon: si:immich
-              - title: AdGuard Home
-                url: https://yourdomain.com/
-                icon: si:adguard
-              - title: Vaultwarden
-                url: https://yourdomain.com/
-                icon: si:vaultwarden
-
           - type: bookmarks
             groups:
               - title: General
@@ -72,22 +50,6 @@ pages:
                     url: https://www.amazon.com/
                   - title: Github
                     url: https://github.com/
-              - title: Entertainment
-                links:
-                  - title: YouTube
-                    url: https://www.youtube.com/
-                  - title: Prime Video
-                    url: https://www.primevideo.com/
-                  - title: Disney+
-                    url: https://www.disneyplus.com/
-              - title: Social
-                links:
-                  - title: Reddit
-                    url: https://www.reddit.com/
-                  - title: Twitter
-                    url: https://twitter.com/
-                  - title: Instagram
-                    url: https://www.instagram.com/
 EOF
 
 echo "${RELEASE}" >"/opt/${APPLICATION}_version.txt"
@@ -110,7 +72,7 @@ Restart=on-failure
 [Install]
 WantedBy=multi-user.target" >$service_path
 
-systemctl enable --now -q glance.service
+systemctl enable -q --now glance.service
 msg_ok "Created Service"
 
 motd_ssh
