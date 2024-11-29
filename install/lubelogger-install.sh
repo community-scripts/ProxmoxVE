a617bc2ad9a4aafb1f2718d50df6d499dd934deb
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-11-29 11:42:21 +0100

    some little fixes

--- install/lubelogger-install.sh
+++ install/lubelogger-install.sh
@@ -16,6 +16,7 @@ update_os
 msg_info "Installing Dependencies"
 $STD apt-get install -y \
     curl \
+    sudo \
     wget \
     mc \
     zip \
@@ -25,11 +26,12 @@ msg_ok "Installed Dependencies"
 
 msg_info "Installing LubeLogger"
 cd /opt
+mkdir -p /opt/lubelogger
 RELEASE=$(curl -s https://api.github.com/repos/hargata/lubelog/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
 RELEASE_TRIMMED=$(echo "${RELEASE}" | tr -d ".")
+cd /opt/lubelogger
 wget -q https://github.com/hargata/lubelog/releases/download/v${RELEASE}/LubeLogger_v${RELEASE_TRIMMED}_linux_x64.zip
-mkdir /opt/lubelogger
-$STD unzip -qq LubeLogger_v${RELEASE_TRIMMED}_linux_x64.zip -d lubelogger
+unzip -q LubeLogger_v${RELEASE_TRIMMED}_linux_x64.zip
 chmod 700 /opt/lubelogger/CarCareTracker
 cp /opt/lubelogger/appsettings.json /opt/lubelogger/appsettings_bak.json
 jq '.Kestrel = {"Endpoints": {"Http": {"Url": "http://0.0.0.0:5000"}}}' /opt/lubelogger/appsettings_bak.json > /opt/lubelogger/appsettings.json
@@ -37,8 +39,8 @@ echo "${RELEASE}" >"/opt/${APPLICATION}_version.txt"
 msg_ok "Installed LubeLogger"
 
 msg_info "Creating Service"
-service_path="/etc/systemd/system/lubelogger.service"
-echo "[Unit]
+cat <<EOF >/etc/systemd/system/lubelogger.service
+[Unit]
 Description=LubeLogger Daemon
 After=network.target
 
@@ -53,7 +55,8 @@ KillMode=process
 Restart=on-failure
 
 [Install]
-WantedBy=multi-user.target" >$service_path
+WantedBy=multi-user.target
+EOF
 
 systemctl enable --now -q lubelogger.service
 msg_ok "Created Service"
@@ -63,7 +66,7 @@ customize
 
 msg_info "Cleaning up"
 rm -rf /opt/lubelogger/appsettings_bak.json
-rm -rf /opt/LubeLogger_v${RELEASE_TRIMMED}_linux_x64.zip
+rm -rf /opt/lubelogger/LubeLogger_v${RELEASE_TRIMMED}_linux_x64.zip
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
 msg_ok "Cleaned"
