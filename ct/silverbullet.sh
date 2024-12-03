ce3aca46d98d233d23c90e5167d6cf42ce9b8990
Dominik Siebel <dominik.siebel@trivago.com>
2024-12-03 16:45:38 +0100

    update copyright and author information

--- ct/silverbullet.sh
+++ ct/silverbullet.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
-# Copyright (c) 2021-2024 tteck
-# Author: tteck (tteckster)
+# Copyright (c) 2021-2024 community-scripts ORG
+# Author: Dominik Siebel (dsiebel)
 # License: MIT
 # https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 
