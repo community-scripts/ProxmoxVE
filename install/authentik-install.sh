8a0e0dbc11bd0882871d95aec128600ec46e2cda
Rémi Bédard-Couture <remi.bc@outlook.com>
2024-12-08 15:49:03 -0500

    Update install/authentik-install.sh
    Co-authored-by: Håvard Gjøby Thom <34199185+havardthom@users.noreply.github.com>

--- install/authentik-install.sh
+++ install/authentik-install.sh
@@ -52,7 +52,7 @@ $STD ./configure --enable-optimizations
 $STD make altinstall
 cd ~
 rm -rf Python-3.12.1
-$STD rm -rf Python.tgz
+rm -rf Python.tgz
 $STD update-alternatives --install /usr/bin/python3 python3 /usr/local/bin/python3.12 1
 msg_ok "Installed Python 3.12"
 
