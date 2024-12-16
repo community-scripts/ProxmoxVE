55495243082dec6f463b181c5b3fb7b306346044
bvdberg01 <74251551+bvdberg01@users.noreply.github.com>
2024-12-16 21:12:27 +0100

    Migrate header & footer to latest version

--- ct/part-db.sh
+++ ct/part-db.sh
@@ -2,28 +2,24 @@
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2024 community-scripts ORG
 # Author: bvdberg01
-# License: MIT
-# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://docs.part-db.de/
 
-function header_info {
-clear
-cat <<"EOF"
-    ____             __        ____  ____ 
-   / __ \____ ______/ /_      / __ \/ __ )
-  / /_/ / __ `/ ___/ __/_____/ / / / __  |
- / ____/ /_/ / /  / /_/_____/ /_/ / /_/ / 
-/_/    \__,_/_/   \__/     /_____/_____/  
-                                          
-EOF
-}
-header_info
-echo -e "Loading..."
+# App Default Values
 APP="Part-DB"
-var_disk="8"
+var_tags="inventory;parts"
 var_cpu="2"
 var_ram="1024"
+var_disk="8"
 var_os="debian"
 var_version="12"
+var_unprivileged="1"
+
+# App Output & Base Settings
+header_info "$APP"
+base_settings
+
+# Core
 variables
 color
 catch_errors
@@ -105,5 +101,6 @@ build_container
 description
 
 msg_ok "Completed Successfully!\n"
-echo -e "${APP} should be reachable by going to the following URL.
-         ${BL}http://${IP}${CL} \n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}${CL}"
