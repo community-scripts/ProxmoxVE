b24185207e6769abc664453fbbe9ec0a362251b9
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-12-18 11:34:40 +0100

    formatting and remove old header

--- ct/part-db.sh
+++ ct/part-db.sh
@@ -24,76 +24,55 @@ variables
 color
 catch_errors
 
-function default_settings() {
-  CT_TYPE="1"
-  PW=""
-  CT_ID=$NEXTID
-  HN=$NSAPP
-  DISK_SIZE="$var_disk"
-  CORE_COUNT="$var_cpu"
-  RAM_SIZE="$var_ram"
-  BRG="vmbr0"
-  NET="dhcp"
-  GATE=""
-  APT_CACHER=""
-  APT_CACHER_IP=""
-  DISABLEIP6="no"
-  MTU=""
-  SD=""
-  NS=""
-  MAC=""
-  VLAN=""
-  SSH="no"
-  VERB="no"
-  echo_default
-}
-
 function update_script() {
-header_info
-check_container_storage
-check_container_resources
-if [[ ! -d /opt/partdb ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
-RELEASE=$(curl -s https://api.github.com/repos/Part-DB/Part-DB-server/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
-if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
-  msg_info "Stopping Service"
-  systemctl stop apache2
-  msg_ok "Stopped Service"
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -d /opt/partdb ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  RELEASE=$(curl -s https://api.github.com/repos/Part-DB/Part-DB-server/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+  if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
+    msg_info "Stopping Service"
+    systemctl stop apache2
+    msg_ok "Stopped Service"
 
-  msg_info "Updating $APP to v${RELEASE}"
-  cd /opt
-  mv /opt/partdb/ /opt/partdb-backup
-  wget -q "https://github.com/Part-DB/Part-DB-server/archive/refs/tags/v${RELEASE}.zip"
-  unzip -q "v${RELEASE}.zip"
-  mv /opt/Part-DB-server-${RELEASE}/ /opt/partdb
+    msg_info "Updating $APP to v${RELEASE}"
+    cd /opt
+    mv /opt/partdb/ /opt/partdb-backup
+    wget -q "https://github.com/Part-DB/Part-DB-server/archive/refs/tags/v${RELEASE}.zip"
+    unzip -q "v${RELEASE}.zip"
+    mv /opt/Part-DB-server-${RELEASE}/ /opt/partdb
 
-  cd /opt/partdb/
-  cp -r "/opt/partdb-backup/.env.local" /opt/partdb/
-  cp -r "/opt/partdb-backup/public/media" /opt/partdb/public/
-  cp -r "/opt/partdb-backup/config/banner.md" /opt/partdb/config/
-  
-  export COMPOSER_ALLOW_SUPERUSER=1
-  composer install --no-dev -o --no-interaction &>/dev/null
-  yarn install &>/dev/null
-  yarn build &>/dev/null
-  php bin/console cache:clear &>/dev/null
-  php bin/console doctrine:migrations:migrate -n &>/dev/null
-  chown -R www-data:www-data /opt/partdb
-  echo "${RELEASE}" >/opt/${APP}_version.txt
-  msg_ok "Updated $APP to v${RELEASE}"
+    cd /opt/partdb/
+    cp -r "/opt/partdb-backup/.env.local" /opt/partdb/
+    cp -r "/opt/partdb-backup/public/media" /opt/partdb/public/
+    cp -r "/opt/partdb-backup/config/banner.md" /opt/partdb/config/
 
-  msg_info "Starting Service"
-  systemctl start apache2
-  msg_ok "Started Service"
+    export COMPOSER_ALLOW_SUPERUSER=1
+    composer install --no-dev -o --no-interaction &>/dev/null
+    yarn install &>/dev/null
+    yarn build &>/dev/null
+    php bin/console cache:clear &>/dev/null
+    php bin/console doctrine:migrations:migrate -n &>/dev/null
+    chown -R www-data:www-data /opt/partdb
+    echo "${RELEASE}" >/opt/${APP}_version.txt
+    msg_ok "Updated $APP to v${RELEASE}"
 
-  msg_info "Cleaning up"
-  rm -r "/opt/v${RELEASE}.zip"
-  rm -r /opt/partdb-backup
-  msg_ok "Cleaned"
-  msg_ok "Updated Successfully"
-else
-  msg_ok "No update required. ${APP} is already at v${RELEASE}"
-fi
-exit
+    msg_info "Starting Service"
+    systemctl start apache2
+    msg_ok "Started Service"
+
+    msg_info "Cleaning up"
+    rm -r "/opt/v${RELEASE}.zip"
+    rm -r /opt/partdb-backup
+    msg_ok "Cleaned"
+    msg_ok "Updated Successfully"
+  else
+    msg_ok "No update required. ${APP} is already at v${RELEASE}"
+  fi
+  exit
 }
 
 start
