e929220535889900394c44a08cf4e02d3d28d5d6
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-12-19 10:12:57 +0100

    Fix Dependencies

--- install/omada-install.sh
+++ install/omada-install.sh
@@ -14,11 +14,12 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y curl
-$STD apt-get install -y sudo
-$STD apt-get install -y mc
-$STD apt-get install -y gnupg
-$STD apt-get install -y jsvc
+$STD apt-get install -y \
+  curl \
+  sudo \
+  mc \
+  gnupg \
+  jsvc
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Azul Zulu"
