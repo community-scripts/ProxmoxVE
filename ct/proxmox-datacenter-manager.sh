1b2a03ea0e2e15377de6d5212d169ea85ecdf991
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-12-25 22:16:56 +0100

    Fix Tag

--- ct/proxmox-datacenter-manager.sh
+++ ct/proxmox-datacenter-manager.sh
@@ -7,7 +7,7 @@ source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/r
 
 # App Default Values
 APP="proxmox-datacenter-manager"
-var_tags="server,datacenter"
+var_tags="datacenter"
 var_cpu="2"
 var_ram="2048"
 var_disk="10"
