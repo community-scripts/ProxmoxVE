8a72bd1b40f473f3ffd436372e2ca861b87bff53
Johanna Rührig <therealvira@gmail.com>
2025-01-01 16:37:57 +0100

    Fix tags.

--- ct/5etools.sh
+++ ct/5etools.sh
@@ -7,7 +7,7 @@ source <(curl -s https://raw.githubusercontent.com/TheRealVira/ProxmoxVE/refs/he
 
 # App Default Values
 APP="5etools"
-TAGS="wiki"
+var_tags="wiki"
 var_cpu="1"
 var_ram="512"
 var_disk="20"
