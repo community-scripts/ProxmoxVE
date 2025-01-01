496792f10216d3b1d9b16421fe6794b193965a12
Johanna Rührig <therealvira@gmail.com>
2025-01-01 21:11:53 +0100

    fixing useradd command.

--- install/mealie-install.sh
+++ install/mealie-install.sh
@@ -84,7 +84,7 @@ export PATH="$POETRY_HOME/bin:$VENV_PATH/bin:$PATH"
 msg_ok "Setting up ENV"
 
 msg_info "Create user account"
-useradd -u 911 -U -d $MEALIE_HOME -s /bin/bash abc
+useradd -u 911 -U -d $MEALIE_HOME abc
 usermod -G users abc
 mkdir $MEALIE_HOME
 msg_ok "Create user account"
