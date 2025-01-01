dea39ee2624e0ae0a192d9bad1f3861f8813860b
Johanna Rührig <TheRealVira@users.noreply.github.com>
2025-01-01 23:00:07 +0100

    Update 5etools.sh

--- ct/5etools.sh
+++ ct/5etools.sh
@@ -46,7 +46,7 @@ function update_script() {
         # Execute Update
         msg_info "Updating base 5etools"
         wget -q "https://github.com/5etools-mirror-3/5etools-src/archive/refs/tags/${RELEASE}.zip
-        unzip "${RELEASE}.zip"
+        unzip -q "${RELEASE}.zip"
         mv "${APP}-src-${RELEASE:1}" "/opt/${APP}/img"
         echo "${RELEASE}" >"/opt/${APP}_version.txt"
         msg_ok "Updated base 5etools"
@@ -74,8 +74,8 @@ function update_script() {
 
         # Execute Update
         msg_info "Updating 5etools images"
-        curl -L "https://github.com/5etools-mirror-2/5etools-img/archive/refs/tags/${IMG_RELEASE}.zip" > "${IMG_RELEASE}.zip"
-        unzip "${IMG_RELEASE}.zip"
+        curl -sS "https://github.com/5etools-mirror-2/5etools-img/archive/refs/tags/${IMG_RELEASE}.zip" > "${IMG_RELEASE}.zip"
+        unzip -q "${IMG_RELEASE}.zip"
         mv "${APP}-img-${IMG_RELEASE:1}" /opt/${APP}/img
         echo "${IMG_RELEASE}" >"/opt/${APP}_IMG_version.txt"
         msg_ok "Updating 5etools images"
