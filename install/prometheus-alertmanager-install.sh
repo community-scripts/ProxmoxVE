0b1778c1b4c6d0862a83a89f4806b838ef1758d9
Andy Grunwald <andy.grunwald@aiven.io>
2025-01-06 17:04:36 +0100

    Alertmanager: Change installation of curl, sudo and mc to a oneliner
    Co-authored-by: Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>

--- install/prometheus-alertmanager-install.sh
+++ install/prometheus-alertmanager-install.sh
@@ -14,8 +14,10 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y curl
-$STD apt-get install -y sudo
+$STD apt-get install -y \
+  curl \
+  sudo \
+  mc
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Prometheus Alertmanager"
