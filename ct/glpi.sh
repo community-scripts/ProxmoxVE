d219666736858ca4945ebf46d89955976a6c118a
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-01-06 15:03:05 +0100

    Update glpi.sh

--- ct/glpi.sh
+++ ct/glpi.sh
@@ -7,7 +7,7 @@ source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/m
 
 # App Default Values
 APP="GLPI"
-var_tags="assat-management;foss"
+var_tags="asset-management;foss"
 var_cpu="2"
 var_ram="2048"
 var_disk="10"
