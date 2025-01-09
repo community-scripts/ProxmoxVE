c9115da871dae58376f4444a8cddf5d3de9b79ec
Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
2025-01-09 15:56:59 +0100

    Update

--- install/calibre-server-install.sh
+++ install/calibre-server-install.sh
@@ -14,22 +14,25 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y imagemagick xvfb libxcomposite1
+$STD apt-get install -y \ 
+  sudo \
+  curl \
+  mc \
+  imagemagick \
+  xvfb \
+  libxcomposite1
 msg_ok "Installed Dependencies"
 
-msg_info "Installing Calibre (latest)"
+msg_info "Installing Calibre"
 # If your container runs as root, you generally do not need to prefix with `sudo`.
 # The official Calibre instructions:
 wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sh /dev/stdin
-msg_ok "Installed Calibre"
-
-msg_info "Creating calibre system user & directories"
 useradd -c "Calibre Server" -d /opt/calibre -s /bin/bash -m calibre
 mkdir -p /opt/calibre/calibre-library
 chown -R calibre:calibre /opt/calibre
-msg_ok "Created calibre user & directories"
+msg_ok "Installed Calibre"
 
-msg_info "Creating systemd service"
+msg_info "Creating Service"
 cat <<EOF >/etc/systemd/system/calibre-server.service
 [Unit]
 Description=Calibre Content Server
