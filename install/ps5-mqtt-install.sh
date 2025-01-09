6e3eea541c2b94b99e5617e11a29b39217c4741e
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2025-01-09 16:18:11 +0100

    Update ps5-mqtt-install.sh

--- install/ps5-mqtt-install.sh
+++ install/ps5-mqtt-install.sh
@@ -19,10 +19,21 @@ $STD apt-get install -y \
   sudo \
   mc \
   jq \
-  nodejs \
   npm
 $STD npm i -g playactor
 msg_ok "Installed Dependencies"
+
+msg_info "Setting up Node.js Repository"
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
+msg_ok "Set up Node.js Repository"
+
+msg_info "Installing Node.js"
+$STD apt-get update
+$STD apt-get install -y nodejs
+msg_ok "Installed Node.js"
+
 
 msg_info "Installing PS5-MQTT"
 RELEASE=$(curl -s https://api.github.com/repos/FunkeyFlo/ps5-mqtt/releases/latest | jq -r '.tag_name')
