ed98ba52f7620711c1b30549351530b4c6b59449
jd-apprentice <contacto@jonathan.com.ar>
2025-01-11 18:41:46 -0300

    fix: git required to execute

--- install/opengist-install.sh
+++ install/opengist-install.sh
@@ -17,6 +17,7 @@ msg_info "Installing Dependencies"
 $STD apt-get install -y curl
 $STD apt-get install -y jq
 $STD apt-get install -y sudo
+$STD apt-get install -y git
 msg_ok "Installed Dependencies"
 
 msg_info "Download Opengist Binary"
