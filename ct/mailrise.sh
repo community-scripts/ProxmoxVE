7e583897da6b59c75e468e69aa2afd45cc992a2f
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-01-15 10:49:28 +0100

    Update mailrise.sh

--- ct/mailrise.sh
+++ ct/mailrise.sh
@@ -3,7 +3,7 @@ source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/m
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: choehn86
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://github.com/YoRyan/mailrise
+# Source: https://github.com/YoRyan/mailrise 
 
 # App Default Values
 APP="Mailrise"
