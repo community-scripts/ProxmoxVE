48a421402c87b79e3685de4b96004daf6ced4f9f
sinofage@gmail.com <sinofage@gmail.com>
2025-01-20 17:50:09 +0200

    test

--- ct/beszel.sh
+++ ct/beszel.sh
@@ -28,11 +28,22 @@ function update_script() {
     header_info
     check_container_storage
     check_container_resources
+
     if [[ ! -d /opt/beszel ]]; then
         msg_error "No ${APP} Installation Found!"
         exit
     fi
+
+    RELEASE=$(curl -fsSL https://github.com/henrygd/beszel/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+    if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
+        msg_info "Updating ${APP} to v${RELEASE}"
+        /opt/beszel/beszel update
+    else
+        msg_ok "No update required. ${APP} is already at v${RELEASE}."
+    fi
+
     msg_error "Beszel should be updated via the user interface."
+    msg_info "Updating ${APP} LXC"
     exit
 }
 
