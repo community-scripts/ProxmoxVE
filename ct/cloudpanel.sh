713401c691d896270918fa11846e545efdbb7a0d
Nícolas Pastorello <nicolaspastorello1@gmail.com>
2025-01-22 16:02:27 -0300

    Create cloudpanel.sh

---
+++ ct/cloudpanel.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Nícolas Pastorello (opastorello)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+# App Default Values
+APP="CloudPanel"
+var_tags="asset-management;hosting-control-panel"
+var_cpu="2"
+var_ram="2048"
+var_disk="10"
+var_os="ubuntu"
+var_version="24.04"
+var_unprivileged="1"
+
+# App Output & Base Settings
+header_info "$APP"
+base_settings
+
+# Core
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+
+  if ! command -v clpctl >/dev/null 2>&1; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}https://${IP}:8443{CL}"
