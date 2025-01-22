fb4f624f95311158ddf20e890f158a794502d548
Nícolas Pastorello <nicolaspastorello1@gmail.com>
2025-01-22 15:36:29 -0300

    Create cloudpanel-install.sh

---
+++ install/cloudpanel-install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Nícolas Pastorello (opastorello)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+  curl \
+  git \
+  sudo \
+  mc \
+  wget
+msg_ok "Installed Dependencies"
+
+msg_info "Installing CloudPanel"
+$STD curl -sS https://installer.cloudpanel.io/ce/v2/install.sh -o install.sh
+$STD bash install.sh
+msg_ok "Installed CloudPanel"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
