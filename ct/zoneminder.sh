e8e0f10cf7832a4eab1770da1d0b0ef466f93eff
connorjfarrell <connorjfarrell@users.noreply.github.com>
2025-01-23 20:38:55 -0600

    Update zoneminder.sh
    Update header metadata

--- ct/zoneminder.sh
+++ ct/zoneminder.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2024 community-scripts ORG
-# Author: [YourUserName]
+# Author: connorjfarrell
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: [SOURCE_URL]
+# Source: https://zoneminder.readthedocs.io/en/latest/installationguide/ubuntu.html#ubuntu-22-04-jammy
 
 # App Default Values
 APP="ZoneMinder"
