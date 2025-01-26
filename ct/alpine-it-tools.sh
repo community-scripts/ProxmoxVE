a9a4272534353b726aadeafd6e0d3ac709870945
NiceDevil <17103076+nicedevil007@users.noreply.github.com>
2025-01-26 16:05:44 +0100

    Update alpine-it-tools.sh

--- ct/alpine-it-tools.sh
+++ ct/alpine-it-tools.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
-# Copyright (c) 2021-2025 tteck
-# Author: tteck (tteckster)
+source <(curl -s https://raw.githubusercontent.com/nicedevil007/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: nicedevil007 (NiceDevil)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 
 # App Default Values
-APP="Alpine-Docker"
-var_tags="docker;alpine"
+APP="Alpine-IT-Tools"
+var_tags="alpine;development"
 var_cpu="1"
 var_ram="256"
 var_disk="0.2"
@@ -24,40 +24,24 @@ color
 catch_errors
 
 function update_script() {
-  if ! apk -e info newt >/dev/null 2>&1; then
-    apk add -q newt
-  fi
-  while true; do
-    CHOICE=$(
-      whiptail --backtitle "Proxmox VE Helper Scripts" --title "SUPPORT" --menu "Select option" 11 58 1 \
-        "1" "Update IT-Tools" 3>&2 2>&1 1>&3
-    )
-    exit_status=$?
-    if [ $exit_status == 1 ]; then
-      clear
-      exit-script
-    fi
     header_info
-    case $CHOICE in
-    1)
-      get_latest_release() {
-          curl -s https://api.github.com/repos/CorentinTh/it-tools/releases/latest | grep '"tag_name":' | cut -d '"' -f4
-      }
-      LATEST_VERSION=$(get_latest_release)
-      DOWNLOAD_URL="https://github.com/CorentinTh/it-tools/releases/download/$LATEST_VERSION/it-tools-${LATEST_VERSION#v}.zip"
-
-      msg_info "Updating IT-Tools to version $LATEST_VERSION"
-      curl -fsSL -o it-tools.zip "$DOWNLOAD_URL"
-      mkdir -p /usr/share/nginx/html
-      rm -rf /usr/share/nginx/html/*
-      unzip -q it-tools.zip -d /tmp/it-tools
-      cp -r /tmp/it-tools/dist/* /usr/share/nginx/html
-      rm -rf /tmp/it-tools
-      rm -f it-tools.zip
-      msg_ok "IT-Tools updated to version $LATEST_VERSION"
-      ;;
-    esac
-  done
+    check_container_storage
+    check_container_resources
+    if [[ ! -d /usr/share/nginx/html ]]; then
+      msg_error "No ${APP} Installation Found!"
+      exit
+    fi
+    RELEASE=$(curl -s https://api.github.com/repos/CorentinTh/it-tools/releases/latest | grep '"tag_name":' | cut -d '"' -f4)
+    DOWNLOAD_URL="https://github.com/CorentinTh/it-tools/releases/download/${RELEASE}/it-tools-${RELEASE#v}.zip"
+    msg_info "Updating ${APP} LXC"
+    curl -fsSL -o it-tools.zip "$DOWNLOAD_URL"
+    mkdir -p /usr/share/nginx/html
+    rm -rf /usr/share/nginx/html/*
+    unzip -q it-tools.zip -d /tmp/it-tools
+    cp -r /tmp/it-tools/dist/* /usr/share/nginx/html
+    rm -rf /tmp/it-tools
+    rm -f it-tools.zip
+    msg_ok "Updated Successfully"
 }
 
 start
@@ -65,3 +49,6 @@ build_container
 description
 
 msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following IP:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}${IP}${CL}"
