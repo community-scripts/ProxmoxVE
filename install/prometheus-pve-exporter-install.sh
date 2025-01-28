8645c8e78870765eeec92ad796e6319d5890aeb3
Andy Grunwald <andygrunwald@gmail.com>
2025-01-28 11:55:59 +0100

    New Script: Prometheus Proxmox VE Exporter
    See https://github.com/prometheus-pve/prometheus-pve-exporter

---
+++ install/prometheus-pve-exporter-install.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Andy Grunwald (andygrunwald)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/prometheus-pve/prometheus-pve-exporter
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+  curl \
+  sudo \
+  mc
+msg_ok "Installed Dependencies"
+
+msg_info "Updating Python3"
+$STD apt-get install -y \
+  python3 \
+  #python3-dev \
+  python3-pip
+rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED
+msg_ok "Updated Python3"
+
+msg_info "Installing Prometheus Proxmox VE Exporter"
+python3 -m pip install --quiet prometheus-pve-exporter
+
+mkdir -p /etc/prometheus-pve-exporter
+cat <<EOF > /etc/prometheus-pve-exporter/pve.yml
+default:
+    user: prometheus@pve
+    password: sEcr3T!
+    verify_ssl: false
+EOF
+
+msg_ok "Installed Prometheus Proxmox VE Exporter"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/prometheus-pve-exporter.service
+[Unit]
+Description=Prometheus Proxmox VE Exporter
+Documentation=https://github.com/znerol/prometheus-pve-exporter
+After=syslog.target network.target
+
+[Service]
+User=root
+Restart=always
+Type=simple
+ExecStart=pve_exporter \
+    --config.file=/etc/prometheus-pve-exporter/pve.yml \
+    --web.listen-address=0.0.0.0:9221
+ExecReload=/bin/kill -HUP \$MAINPID
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable -q --now prometheus-pve-exporter
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
