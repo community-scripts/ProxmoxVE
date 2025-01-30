7547bc7377baa44f0fa32927fd7498c5ceb9c21e
Brian Ketelsen <bketelsen@gmail.com>
2025-01-30 10:51:27 -0500

    chore: build up vars

--- codemods/build_container.sh
+++ codemods/build_container.sh
@@ -11,26 +11,31 @@ build_container() {
   TEMP_DIR=$(mktemp -d)
   pushd $TEMP_DIR >/dev/null
   if [ "$var_os" == "alpine" ]; then
-    export FUNCTIONS_FILE_PATH="$(curl -s https://raw.githubusercontent.com/bketelsen/IncusScripts/main/misc/alpine-install.func)"
+    curl -s https://raw.githubusercontent.com/bketelsen/IncusScripts/main/misc/alpine-install.func -o install.func
   else
-    export FUNCTIONS_FILE_PATH="$(curl -s https://raw.githubusercontent.com/bketelsen/IncusScripts/main/misc/install.func)"
+    curl -s https://raw.githubusercontent.com/bketelsen/IncusScripts/main/misc/install.func -o install.func
   fi
-  export CACHER="$APT_CACHER"
-  export CACHER_IP="$APT_CACHER_IP"
-  export tz="$timezone"
-  export DISABLEIPV6="$DISABLEIP6"
-  export APPLICATION="$APP"
-  export app="$NSAPP"
-  export PASSWORD="$PW"
-  export VERBOSE="$VERB"
-  export SSH_ROOT="${SSH}"
-  export SSH_AUTHORIZED_KEY
-  export CTID="$CT_ID"
-  export CTTYPE="$CT_TYPE"
-  export PCT_OSTYPE="$var_os"
-  export PCT_OSVERSION="$var_version"
-  export PCT_DISK_SIZE="$DISK_SIZE"
-  export PCT_OPTIONS="
+  # if [ "$var_os" == "alpine" ]; then
+  #   export FUNCTIONS_FILE_PATH="$(curl -s https://raw.githubusercontent.com/bketelsen/IncusScripts/main/misc/alpine-install.func)"
+  # else
+  #   export FUNCTIONS_FILE_PATH="$(curl -s https://raw.githubusercontent.com/bketelsen/IncusScripts/main/misc/install.func)"
+  # fi
+  echo export CACHER="$APT_CACHER" >>install.func
+  echo export CACHER_IP="$APT_CACHER_IP" >>install.func
+  echo export tz="$timezone" >>install.func
+  echo export DISABLEIPV6="$DISABLEIP6" >>install.func
+  echo export APPLICATION="$APP" >>install.func
+  echo export app="$NSAPP" >>install.func
+  echo export PASSWORD="$PW" >>install.func
+  echo export VERBOSE="$VERB" >>install.func
+  echo export SSH_ROOT="${SSH}" >>install.func
+  echo export SSH_AUTHORIZED_KEY "${SSH_AUTHORIZED_KEY}" >>install.func
+  echo export CTID="$CT_ID" >>install.func
+  echo export CTTYPE="$CT_TYPE" >>install.func
+  echo export PCT_OSTYPE="$var_os"  >>install.func
+  echo export PCT_OSVERSION="$var_version" >>install.func
+  echo export PCT_DISK_SIZE="$DISK_SIZE"  >>install.func
+  echo export PCT_OPTIONS="
     -features $FEATURES
     -hostname $HN
     -tags $TAGS
@@ -42,58 +47,59 @@ build_container() {
     -memory $RAM_SIZE
     -unprivileged $CT_TYPE
     $PW
-  "
+  " >>install.func
   # This executes create_lxc.sh and creates the container and .conf file
   bash -c "$(wget -qLO - https://raw.githubusercontent.com/bketelsen/IncusScripts/main/ct/create_lxc.sh)" || exit
 
-  LXC_CONFIG=/etc/pve/lxc/${CTID}.conf
-  if [ "$CT_TYPE" == "0" ]; then
-    cat <<EOF >>$LXC_CONFIG
-# USB passthrough
-lxc.cgroup2.devices.allow: a
-lxc.cap.drop:
-lxc.cgroup2.devices.allow: c 188:* rwm
-lxc.cgroup2.devices.allow: c 189:* rwm
-lxc.mount.entry: /dev/serial/by-id  dev/serial/by-id  none bind,optional,create=dir
-lxc.mount.entry: /dev/ttyUSB0       dev/ttyUSB0       none bind,optional,create=file
-lxc.mount.entry: /dev/ttyUSB1       dev/ttyUSB1       none bind,optional,create=file
-lxc.mount.entry: /dev/ttyACM0       dev/ttyACM0       none bind,optional,create=file
-lxc.mount.entry: /dev/ttyACM1       dev/ttyACM1       none bind,optional,create=file
-EOF
-  fi
+#   LXC_CONFIG=/etc/pve/lxc/${CTID}.conf
+#   if [ "$CT_TYPE" == "0" ]; then
+#     cat <<EOF >>$LXC_CONFIG
+# # USB passthrough
+# lxc.cgroup2.devices.allow: a
+# lxc.cap.drop:
+# lxc.cgroup2.devices.allow: c 188:* rwm
+# lxc.cgroup2.devices.allow: c 189:* rwm
+# lxc.mount.entry: /dev/serial/by-id  dev/serial/by-id  none bind,optional,create=dir
+# lxc.mount.entry: /dev/ttyUSB0       dev/ttyUSB0       none bind,optional,create=file
+# lxc.mount.entry: /dev/ttyUSB1       dev/ttyUSB1       none bind,optional,create=file
+# lxc.mount.entry: /dev/ttyACM0       dev/ttyACM0       none bind,optional,create=file
+# lxc.mount.entry: /dev/ttyACM1       dev/ttyACM1       none bind,optional,create=file
+# EOF
+#   fi
 
-  if [ "$CT_TYPE" == "0" ]; then
-    if [[ "$APP" == "Channels" || "$APP" == "Emby" || "$APP" == "ErsatzTV" || "$APP" == "Frigate" || "$APP" == "Jellyfin" || "$APP" == "Plex" || "$APP" == "Scrypted" || "$APP" == "Tdarr" || "$APP" == "Unmanic" || "$APP" == "Ollama" ]]; then
-      cat <<EOF >>$LXC_CONFIG
-# VAAPI hardware transcoding
-lxc.cgroup2.devices.allow: c 226:0 rwm
-lxc.cgroup2.devices.allow: c 226:128 rwm
-lxc.cgroup2.devices.allow: c 29:0 rwm
-lxc.mount.entry: /dev/fb0 dev/fb0 none bind,optional,create=file
-lxc.mount.entry: /dev/dri dev/dri none bind,optional,create=dir
-lxc.mount.entry: /dev/dri/renderD128 dev/dri/renderD128 none bind,optional,create=file
-EOF
-    fi
-  else
-    if [[ "$APP" == "Channels" || "$APP" == "Emby" || "$APP" == "ErsatzTV" || "$APP" == "Frigate" || "$APP" == "Jellyfin" || "$APP" == "Plex" || "$APP" == "Scrypted" || "$APP" == "Tdarr" || "$APP" == "Unmanic" || "$APP" == "Ollama" ]]; then
-      if [[ -e "/dev/dri/renderD128" ]]; then
-        if [[ -e "/dev/dri/card0" ]]; then
-          cat <<EOF >>$LXC_CONFIG
-# VAAPI hardware transcoding
-dev0: /dev/dri/card0,gid=44
-dev1: /dev/dri/renderD128,gid=104
-EOF
-        else
-          cat <<EOF >>$LXC_CONFIG
-# VAAPI hardware transcoding
-dev0: /dev/dri/card1,gid=44
-dev1: /dev/dri/renderD128,gid=104
-EOF
-        fi
-      fi
-    fi
-  fi
+#   if [ "$CT_TYPE" == "0" ]; then
+#     if [[ "$APP" == "Channels" || "$APP" == "Emby" || "$APP" == "ErsatzTV" || "$APP" == "Frigate" || "$APP" == "Jellyfin" || "$APP" == "Plex" || "$APP" == "Scrypted" || "$APP" == "Tdarr" || "$APP" == "Unmanic" || "$APP" == "Ollama" ]]; then
+#       cat <<EOF >>$LXC_CONFIG
+# # VAAPI hardware transcoding
+# lxc.cgroup2.devices.allow: c 226:0 rwm
+# lxc.cgroup2.devices.allow: c 226:128 rwm
+# lxc.cgroup2.devices.allow: c 29:0 rwm
+# lxc.mount.entry: /dev/fb0 dev/fb0 none bind,optional,create=file
+# lxc.mount.entry: /dev/dri dev/dri none bind,optional,create=dir
+# lxc.mount.entry: /dev/dri/renderD128 dev/dri/renderD128 none bind,optional,create=file
+# EOF
+#     fi
+#   else
+#     if [[ "$APP" == "Channels" || "$APP" == "Emby" || "$APP" == "ErsatzTV" || "$APP" == "Frigate" || "$APP" == "Jellyfin" || "$APP" == "Plex" || "$APP" == "Scrypted" || "$APP" == "Tdarr" || "$APP" == "Unmanic" || "$APP" == "Ollama" ]]; then
+#       if [[ -e "/dev/dri/renderD128" ]]; then
+#         if [[ -e "/dev/dri/card0" ]]; then
+#           cat <<EOF >>$LXC_CONFIG
+# # VAAPI hardware transcoding
+# dev0: /dev/dri/card0,gid=44
+# dev1: /dev/dri/renderD128,gid=104
+# EOF
+#         else
+#           cat <<EOF >>$LXC_CONFIG
+# # VAAPI hardware transcoding
+# dev0: /dev/dri/card1,gid=44
+# dev1: /dev/dri/renderD128,gid=104
+# EOF
+#         fi
+#       fi
+#     fi
+#   fi
 
+  incus file push install.func "$app"/root/install.func
   # This starts the container and executes <app>-install.sh
   msg_info "Starting Incus Container"
   incus start "$app"
@@ -104,8 +110,8 @@ EOF
 http://dl-cdn.alpinelinux.org/alpine/latest-stable/main
 http://dl-cdn.alpinelinux.org/alpine/latest-stable/community
 EOF'
-    incus exec "$app" --env=FUNCTIONS_FILE_PATH="$FUNCTIONS_FILE_PATH"  -- ash -c "apk add bash >/dev/null"
+    incus exec "$app" --env=FUNCTIONS_FILE_PATH=/root/install.func  -- ash -c "apk add bash >/dev/null"
   fi
-  incus shell "$app" --env=FUNCTIONS_FILE_PATH="$FUNCTIONS_FILE_PATH" -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/bketelsen/IncusScripts/main/install/$var_install.sh)" || exit
+  incus shell "$app" --env=FUNCTIONS_FILE_PATH=/root/install.func -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/bketelsen/IncusScripts/main/install/$var_install.sh)" || exit
 
 }
