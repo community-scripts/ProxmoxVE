856fc363bf873c74a5fef680037f997af0e8f883
Brian Ketelsen <bketelsen@gmail.com>
2025-01-31 09:51:06 -0500

    fix: use hn for IP

--- codemods/description.sh
+++ codemods/description.sh
@@ -1,5 +1,5 @@
 description() {
-  IP=$(incus exec "$app" ip a s dev eth0 | awk '/inet / {print $2}' | cut -d/ -f1)
+  IP=$(incus exec "$HN" ip a s dev eth0 | awk '/inet / {print $2}' | cut -d/ -f1)
   if [[ -f /etc/systemd/system/ping-instances.service ]]; then
     systemctl start ping-instances.service
   fi
