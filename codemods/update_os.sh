01df65d3a6d5f4bdbe564a9cae5292992bea7741
Brian Ketelsen <bketelsen@gmail.com>
2025-01-31 11:19:36 -0500

    chore: FE start

---
+++ codemods/update_os.sh
@@ -0,0 +1,20 @@
+update_os() {
+  msg_info "Updating Container OS"
+  if [[ "$CACHER" == "yes" ]]; then
+    echo "Acquire::http::Proxy-Auto-Detect \"/usr/local/bin/apt-proxy-detect.sh\";" >/etc/apt/apt.conf.d/00aptproxy
+    cat <<EOF >/usr/local/bin/apt-proxy-detect.sh
+#!/bin/bash
+if nc -w1 -z "${CACHER_IP}" 3142; then
+  echo -n "http://${CACHER_IP}:3142"
+else
+  echo -n "DIRECT"
+fi
+EOF
+  chmod +x /usr/local/bin/apt-proxy-detect.sh
+  fi
+  $STD apt-get update
+  $STD apt-get -o Dpkg::Options::="--force-confold" -y dist-upgrade
+  rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED
+  $STD apt-get install -y wget
+  msg_ok "Updated Container OS"
+}
