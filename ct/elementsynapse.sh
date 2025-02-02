6aa77b764a13535fbcca07bcd2e95695917a7539
tremor021 <arezina.slavisa@gmail.com>
2025-02-02 23:35:09 +0100

    add element synapse script

--- ct/elementsynapse.sh
+++ ct/elementsynapse.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
-# Author: Andy Grunwald (andygrunwald)
+# Author: tremor021
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://github.com/prometheus-pve/prometheus-pve-exporter
+# Source: https://github.com/element-hq/synapse
 
 # App Default Values
-APP="Prometheus-PVE-Exporter"
-var_tags="monitoring"
+APP="Element Synapse"
+var_tags="server"
 var_cpu="1"
 var_ram="1024"
-var_disk="2"
+var_disk="4"
 var_os="debian"
 var_version="12"
 var_unprivileged="1"
@@ -19,7 +19,7 @@ var_unprivileged="1"
 header_info "$APP"
 base_settings
 
-# Core
+# Core 
 variables
 color
 catch_errors
@@ -28,22 +28,14 @@ function update_script() {
     header_info
     check_container_storage
     check_container_resources
-    if [[ ! -f /etc/systemd/system/prometheus-pve-exporter.service ]]; then
+    if [[ ! -d /etc/matrix-synapse ]]; then 
         msg_error "No ${APP} Installation Found!"
         exit
     fi
-    msg_info "Stopping ${APP}"
-    systemctl stop prometheus-pve-exporter
-    msg_ok "Stopped ${APP}"
-
-    msg_info "Updating ${APP}"
-    pip install prometheus-pve-exporter --default-timeout=300 --upgrade --root-user-action=ignore &>/dev/null
-    msg_ok "Updated ${APP}"
-
-    msg_info "Starting ${APP}"
-    systemctl start prometheus-pve-exporter
-    msg_ok "Started ${APP}"
-    msg_ok "Updated Successfully"
+    msg_info "Updating $APP LXC"
+    apt-get update &>/dev/null
+    apt-get -y upgrade &>/dev/null
+    msg_ok "Updated $APP LXC"
     exit
 }
 
@@ -54,4 +46,4 @@ description
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:9221${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8008${CL}"
---
+++ install/elementsynapse-install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: tremor021
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt install -y lsb-release wget apt-transport-https debconf-utils
+msg_ok "Installed Dependencies"
+
+wget -O /usr/share/keyrings/matrix-org-archive-keyring.gpg https://packages.matrix.org/debian/matrix-org-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/matrix-org-archive-keyring.gpg] https://packages.matrix.org/debian/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/matrix-org.list
+$STD apt update
+
+msg_info "Installing Element Synapse"
+echo "matrix-synapse-py3 matrix-synapse/server-name string matrix" | debconf-set-selections
+echo "matrix-synapse-py3 matrix-synapse/report-stats boolean false" | debconf-set-selections
+$STD apt install matrix-synapse-py3 -y
+systemctl stop matrix-synapse
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/matrix-synapse/homeserver.yaml
+systemctl start matrix-synapse
+msg_ok "Installed Element Synapse"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
