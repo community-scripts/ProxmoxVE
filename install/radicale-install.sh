c0901ac27bed82271c653a3032928a7482944226
tremor021 <arezina.slavisa@gmail.com>
2025-02-03 10:14:56 +0100

    fixes

--- install/radicale-install.sh
+++ install/radicale-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-#Copyright (c) 2021-2025 community-scripts ORG
-# Author: Michel Roegl-Brunner (michelroegl-brunner)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Slaviša Arežina (tremor021)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
