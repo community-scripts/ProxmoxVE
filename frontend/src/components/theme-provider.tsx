d46f1e4e954c750bcfe1204f46c9c9fac221aca7
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-02-06 16:58:51 +0100

    fix toggle

--- frontend/src/components/theme-provider.tsx
+++ frontend/src/components/theme-provider.tsx
@@ -2,10 +2,21 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
+import { useEffect, useState } from "react";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <div className="bg-black h-screen w-screen" />;
+  }
+
   return (
-    <NextThemesProvider defaultTheme="dark" enableSystem {...props}>
+    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem {...props}>
       {children}
     </NextThemesProvider>
   );
