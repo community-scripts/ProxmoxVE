f3bc197e25c240c8cd2fb5f4f3c248af33a5ad74
Andy Grunwald <andygrunwald@gmail.com>
2025-02-06 15:34:18 +0100

    Apache Tika: Add `update_script`

--- ct/apache-tika.sh
+++ ct/apache-tika.sh
@@ -25,7 +25,37 @@ variables
 color
 catch_errors
 
-# TODO Add update_script() function
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -f /etc/systemd/system/apache-tika.service ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  RELEASE="$(wget -qO- https://dlcdn.apache.org/tika/ | grep -oP '(?<=href=")[0-9]+\.[0-9]+\.[0-9]+(?=/")' | sort -V | tail -n1)"
+  if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
+    msg_info "Stopping ${APP}"
+    systemctl stop apache-tika
+    msg_ok "Stopped ${APP}"
+
+    msg_info "Updating ${APP} to v${RELEASE}"
+    cd /opt/apache-tika
+    wget -q "https://dlcdn.apache.org/tika/${RELEASE}/tika-server-standard-${RELEASE}.jar"
+    mv --force tika-server-standard.jar tika-server-standard-prev-version.jar
+    mv tika-server-standard-${RELEASE}.jar tika-server-standard.jar
+    echo "${RELEASE}" >/opt/${APP}_version.txt
+    msg_ok "Updated ${APP} to v${RELEASE}"
+
+    msg_info "Starting ${APP}"
+    systemctl start apache-tika
+    msg_ok "Started ${APP}"
+    msg_ok "Updated Successfully"
+  else
+    msg_ok "No update required. ${APP} is already at v${RELEASE}"
+  fi
+  exit
+}
 
 start
 build_container
