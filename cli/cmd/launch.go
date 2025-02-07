b15a536b95e85f35309c9387f0dbf1993933f591
Brian Ketelsen <bketelsen@gmail.com>
2025-02-07 15:29:28 -0500

    docs: work

--- cli/cmd/launch.go
+++ cli/cmd/launch.go
@@ -53,12 +53,7 @@ func (c *cmdLaunch) Command() *cobra.Command {
 	cmd.Args = cobra.ExactArgs(2)
 
 	cmd.Long =
-		`A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`
+		`launch a container`
 	cmd.RunE = c.Run
 
 	return cmd
--- cli/cmd/search.go
+++ cli/cmd/search.go
@@ -40,12 +40,7 @@ func (c *cmdSearch) Command() *cobra.Command {
 	cmd.Short = "search catalog"
 	cmd.Args = cobra.MinimumNArgs(1)
 	cmd.Long =
-		`A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`
+		`search application catalog`
 	cmd.RunE = c.Run
 
 	return cmd
