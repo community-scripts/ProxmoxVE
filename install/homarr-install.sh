ad4fc247ff51b3c097a01b92a59bafcb8ff840cb
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-02-13 07:59:48 +0100

    Update homarr-install.sh

--- install/homarr-install.sh
+++ install/homarr-install.sh
@@ -87,7 +87,7 @@ msg_ok "Installed Homarr"
 
 msg_info "Creating Services"
 cat <<EOF >/opt/run_homarr.sh
-  DB_DIALECT='sqlite'
+  export DB_DIALECT='sqlite'
   node /opt/homarr_db/migrations/$DB_DIALECT/migrate.cjs /opt/homarr_db/migrations/$DB_DIALECT
   export AUTH_SECRET=$(openssl rand -base64 32)
   export HOSTNAME=$(ip route get 1.1.1.1 | grep -oP 'src \K[^ ]+')
