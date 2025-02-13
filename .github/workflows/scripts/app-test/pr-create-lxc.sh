54e56fb17f96a886efbc37f44aa3c208dfc382f0
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2025-02-13 11:04:00 +0100

    Remove unwanted output from workflow

--- .github/workflows/scripts/app-test/pr-create-lxc.sh
+++ .github/workflows/scripts/app-test/pr-create-lxc.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-# Copyright (c) 2021-2025 community-scripts ORG
-# Author: michelroegl-brunner
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-
 color() {
   return
 }
@@ -11,7 +7,7 @@ catch_errors() {
   trap 'error_handler $LINENO "$BASH_COMMAND"' ERR
 }
 
-
+# This function handles errors
 error_handler() {
     local exit_code="$?"
   local line_number="$1"
@@ -52,6 +48,26 @@ if [ -z "$VALIDTMP" ]; then
   exit 1
 fi
 
+#function select_storage() {
+#  local CLASS=$1
+#  local CONTENT
+#  
+#  case $CLASS in
+#    container) CONTENT='rootdir' ;;
+#    template) CONTENT='vztmpl' ;;
+#    *) msg_error "Invalid storage class." && exit 201 ;;
+#  esac
+#
+#  # Get the first available storage tag
+#  local STORAGE=$(pvesm status -content $CONTENT | awk 'NR==2 {print $1}')
+#
+#  echo "STORAGE = $STORAGE"
+#
+#  if [[ -z "$STORAGE" ]]; then
+#    msg_error "No available storage found for $CLASS."
+#    exit 202
+#  fi
+#}
 function select_storage() {
   local CLASS=$1
   local CONTENT
@@ -68,6 +84,7 @@ function select_storage() {
   *) false || { msg_error "Invalid storage class."; exit 201; };;
   esac
   
+  # This Queries all storage locations
   local -a MENU
   while read -r line; do
     local TAG=$(echo $line | awk '{print $1}')
@@ -81,6 +98,7 @@ function select_storage() {
     MENU+=("$TAG" "$ITEM" "OFF")
   done < <(pvesm status -content $CONTENT | awk 'NR>1')
   
+  # Select storage location
   if [ $((${#MENU[@]}/3)) -eq 1 ]; then
     printf ${MENU[0]}
   else
@@ -94,8 +112,10 @@ function select_storage() {
 [[ "${CTID:-}" ]] || { msg_error "You need to set 'CTID' variable."; exit 203; }
 [[ "${PCT_OSTYPE:-}" ]] || { msg_error "You need to set 'PCT_OSTYPE' variable."; exit 204; }
 
+# Test if ID is valid
 [ "$CTID" -ge "100" ] || { msg_error "ID cannot be less than 100."; exit 205; }
 
+# Test if ID is in use
 if pct status $CTID &>/dev/null; then
   echo -e "ID '$CTID' is already in use."
   unset CTID
@@ -103,17 +123,12 @@ if pct status $CTID &>/dev/null; then
   exit 206
 fi
 
-
 TEMPLATE_STORAGE=$(select_storage template) || exit
-msg_ok "Using  $TEMPLATE_STORAGE   for Template Storage."
-
 
 CONTAINER_STORAGE=$(select_storage container) || exit
-msg_ok "Using  $CONTAINER_STORAGE   for Container Storage."
 
-msg_info "Updating LXC Template List"
 pveam update >/dev/null
-msg_ok "Updated LXC Template List"
+
 
 TEMPLATE_SEARCH=${PCT_OSTYPE}-${PCT_OSVERSION:-}
 mapfile -t TEMPLATES < <(pveam available -section system | sed -n "s/.*\($TEMPLATE_SEARCH.*\)/\1/p" | sort -t - -k 2 -V)
@@ -124,35 +139,29 @@ TEMPLATE_PATH="/var/lib/vz/template/cache/$TEMPLATE"
 
 if ! pveam list "$TEMPLATE_STORAGE" | grep -q "$TEMPLATE"; then
   [[ -f "$TEMPLATE_PATH" ]] && rm -f "$TEMPLATE_PATH"
-  msg_info "Downloading LXC Template"
   pveam download "$TEMPLATE_STORAGE" "$TEMPLATE" >/dev/null ||
     { msg_error "A problem occurred while downloading the LXC template."; exit 208; }
-  msg_ok "Downloaded LXC Template"
 fi
 
 
 grep -q "root:100000:65536" /etc/subuid || echo "root:100000:65536" >> /etc/subuid
 grep -q "root:100000:65536" /etc/subgid || echo "root:100000:65536" >> /etc/subgid
 
-
 PCT_OPTIONS=(${PCT_OPTIONS[@]:-${DEFAULT_PCT_OPTIONS[@]}})
 [[ " ${PCT_OPTIONS[@]} " =~ " -rootfs " ]] || PCT_OPTIONS+=(-rootfs "$CONTAINER_STORAGE:${PCT_DISK_SIZE:-8}")
 
-echo "${PCT_OPTIONS[@]}"
 
-
-msg_info "Creating LXC Container"
   if ! pct create "$CTID" "${TEMPLATE_STORAGE}:vztmpl/${TEMPLATE}" "${PCT_OPTIONS[@]}" &>/dev/null; then
       [[ -f "$TEMPLATE_PATH" ]] && rm -f "$TEMPLATE_PATH"
       
-    msg_ok "Template integrity check completed"
+
     pveam download "$TEMPLATE_STORAGE" "$TEMPLATE" >/dev/null ||    
       { msg_error "A problem occurred while re-downloading the LXC template."; exit 208; }
     
-    msg_ok "Re-downloaded LXC Template"
+
     if ! pct create "$CTID" "${TEMPLATE_STORAGE}:vztmpl/${TEMPLATE}" "${PCT_OPTIONS[@]}" &>/dev/null; then
         msg_error "A problem occurred while trying to create container after re-downloading template."
       exit 200
     fi
   fi
-msg_ok "LXC Container  $CTID   was successfully created."
+
