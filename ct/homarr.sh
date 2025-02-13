16337ad28088f181fcfd471ce4e22483a5338f15
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-02-13 16:08:37 +0100

    Update homarr.sh

--- ct/homarr.sh
+++ ct/homarr.sh
@@ -5,7 +5,6 @@ source <(curl -s https://raw.githubusercontent.com/CrazyWolf13/ProxmoxVE/refs/he
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://homarr.dev/
 
-# App Default Values
 APP="Homarr"
 var_tags="arr;dashboard"
 var_cpu="2"
@@ -15,11 +14,8 @@ var_os="debian"
 var_version="12"
 var_unprivileged="1"
 
-# App Output & Base Settings
 header_info "$APP"
-base_settings
 
-# Core
 variables
 color
 catch_errors
