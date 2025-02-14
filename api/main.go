bf31d40366acd49ba90a0d064d0b64faa3dbd9ef
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2025-02-14 10:22:59 +0100

    Add pagination to API

--- api/main.go
+++ api/main.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2021-2025 community-scripts ORG
+// Author: Michel Roegl-Brunner (michelroegl-brunner)
+// License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
 package main
 
 import (
