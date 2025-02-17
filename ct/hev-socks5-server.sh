22ab279d30d66c562231886c4f6fb63b815bbccc
Miguel <miviro0804@gmail.com>
2025-02-17 18:17:12 +0100

    last test, uyndo this commit

--- ct/hev-socks5-server.sh
+++ ct/hev-socks5-server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/miviro/ProxmoxVE/refs/heads/hev-socks5-branch/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: miviro
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
