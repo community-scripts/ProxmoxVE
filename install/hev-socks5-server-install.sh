328aaad22fded52e3880b703ca1195a6e42ff27a
Miguel <miviro0804@gmail.com>
2025-02-17 18:15:10 +0100

    add username ct

--- install/hev-socks5-server-install.sh
+++ install/hev-socks5-server-install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2025 community-scripts ORG
-# Author: [YourUserName]
+# Author: miviro
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: [SOURCE_URL]
+# Source: https://github.com/heiher/hev-socks5-server
 
 # Import Functions und Setup
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
