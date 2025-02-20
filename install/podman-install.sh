4edbdaa92ae849b5ebcfe501870730605f75705c
jaminmc <1310376+jaminmc@users.noreply.github.com>
2025-02-20 13:25:03 -0500

    Added option to install Portainer or Portainer agent in Podman

--- install/podman-install.sh
+++ install/podman-install.sh
@@ -19,6 +19,13 @@ $STD apt-get install -y sudo
 $STD apt-get install -y mc
 msg_ok "Installed Dependencies"
 
+get_latest_release() {
+  curl -sL https://api.github.com/repos/$1/releases/latest | grep '"tag_name":' | cut -d'"' -f4
+}
+
+PORTAINER_LATEST_VERSION=$(get_latest_release "portainer/portainer")
+PORTAINER_AGENT_LATEST_VERSION=$(get_latest_release "portainer/agent")
+
 if $STD mount | grep 'on / type zfs' > null && echo "ZFS"; then
     msg_info "Enabling ZFS support."
     mkdir -p /etc/containers
@@ -47,6 +54,35 @@ $STD apt-get -y install podman
 $STD systemctl enable --now podman.socket
 echo -e 'unqualified-search-registries=["docker.io"]' >> /etc/containers/registries.conf
 msg_ok "Installed Podman"
+
+read -r -p "Would you like to add Portainer? <y/N> " prompt
+if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
+  msg_info "Installing Portainer $PORTAINER_LATEST_VERSION"
+  podman volume create portainer_data >/dev/null
+  $STD podman run -d \
+    -p 8000:8000 \
+    -p 9443:9443 \
+    --name=portainer \
+    --restart=always \
+    -v /run/podman/podman.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:latest
+  msg_ok "Installed Portainer $PORTAINER_LATEST_VERSION"
+else
+  read -r -p "Would you like to add the Portainer Agent? <y/N> " prompt
+  if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
+    msg_info "Installing Portainer agent $PORTAINER_AGENT_LATEST_VERSION"
+    $STD podman run -d \
+      -p 9001:9001 \
+      --name portainer_agent \
+      --restart=always \
+      -v /run/podman/podman.sock:/var/run/docker.sock \
+      -v /var/lib/containers/storage/volumes:/var/lib/docker/volumes \
+      portainer/agent
+    msg_ok "Installed Portainer Agent $PORTAINER_AGENT_LATEST_VERSION"
+  fi
+fi
+
 
 motd_ssh
 customize
