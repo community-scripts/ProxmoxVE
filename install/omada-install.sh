1c8ed5a7ca20bdc665146be927e4eada433727f9
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-02-24 13:11:08 +0100

    Update omada-install.sh

--- install/omada-install.sh
+++ install/omada-install.sh
@@ -14,13 +14,17 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y \
-  curl \
-  sudo \
-  mc \
-  gnupg \
-  jsvc
+$STD apt-get install -y curl sudo mc gnupg jsvc
 msg_ok "Installed Dependencies"
+
+msg_info "Checking CPU Features"
+if lscpu | grep -q 'avx'; then
+    USE_AVX=true
+    msg_ok "AVX detected: Using MongoDB 8.0"
+else
+    USE_AVX=false
+    msg_ok "No AVX detected: Using MongoDB 4.4"
+fi
 
 msg_info "Installing Azul Zulu"
 wget -qO /etc/apt/trusted.gpg.d/zulu-repo.asc "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xB1998361219BD9C9"
@@ -31,17 +35,19 @@ $STD apt-get -y install zulu8-jdk
 msg_ok "Installed Azul Zulu"
 
 msg_info "Installing MongoDB"
-libssl=$(curl -fsSL "http://security.ubuntu.com/ubuntu/pool/main/o/openssl/" | grep -o 'libssl1\.1_1\.1\.1f-1ubuntu2\.2[^"]*amd64\.deb' | head -n1)
-wget -qL http://security.ubuntu.com/ubuntu/pool/main/o/openssl/$libssl
-$STD dpkg -i $libssl
-wget -qL https://repo.mongodb.org/apt/ubuntu/dists/bionic/mongodb-org/3.6/multiverse/binary-amd64/mongodb-org-server_3.6.23_amd64.deb
-$STD dpkg -i mongodb-org-server_3.6.23_amd64.deb
+if [[ "$USE_AVX" == true ]]; then
+    wget -qL https://repo.mongodb.org/apt/debian/dists/bookworm/mongodb-org/8.0/main/binary-amd64/mongodb-org-server_8.0.4_amd64.deb
+    $STD dpkg -i mongodb-org-server_8.0.4_amd64.deb
+else
+    wget -qL https://repo.mongodb.org/apt/debian/dists/buster/mongodb-org/4.4/main/binary-amd64/mongodb-org-server_4.4.29_amd64.deb
+    $STD dpkg -i mongodb-org-server_4.4.29_amd64.deb
+fi
 msg_ok "Installed MongoDB"
 
+msg_info "Installing Omada Controller"
 latest_url=$(curl -s "https://support.omadanetworks.com/en/product/omada-software-controller/?resourceType=download" | grep -o 'https://static\.tp-link\.com/upload/software/[^"]*linux_x64[^"]*\.deb' | head -n 1)
 latest_version=$(basename "$latest_url")
 
-msg_info "Installing Omada Controller"
 wget -qL ${latest_url}
 $STD dpkg -i ${latest_version}
 msg_ok "Installed Omada Controller"
@@ -50,7 +56,7 @@ motd_ssh
 customize
 
 msg_info "Cleaning up"
-rm -rf ${latest_version} mongodb-org-server_3.6.23_amd64.deb zulu-repo_1.0.0-3_all.deb $libssl
+rm -rf ${latest_version} mongodb-org-server_* zulu-repo_1.0.0-3_all.deb
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
 msg_ok "Cleaned"
