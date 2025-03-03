67c2543e29dc38769dc8c59ae7151e313d9f4a4a
bvdberg01 <74251551+bvdberg01@users.noreply.github.com>
2025-03-03 19:01:34 +0100

    Add sources and replace tag

--- ct/pterodactyl-wings.sh
+++ ct/pterodactyl-wings.sh
@@ -6,7 +6,7 @@ source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/m
 # Source: https://github.com/pterodactyl-dev/wings
 
 APP="Pterodactyl-Wings"
-var_tags="Gaming"
+var_tags="gaming"
 var_cpu="2"
 var_ram="4096"
 var_disk="8"
