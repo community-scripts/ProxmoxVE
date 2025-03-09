616cf8afcefc7c3cc97c8243d7891e6f3989b351
Slaviša Arežina <arezina.slavisa@gmail.com>
2025-03-09 14:01:11 +0100

    another safety layer

--- ct/openwebui.sh
+++ ct/openwebui.sh
@@ -31,6 +31,7 @@ function update_script() {
   cd /opt/open-webui
   mkdir /opt/open-webui-backup
   cp -rf /opt/open-webui/backend/data /opt/open-webui-backup
+  $STD git stash
   $STD git reset --hard
   output=$(git pull --no-rebase)
   if echo "$output" | grep -q "Already up to date."; then
@@ -44,6 +45,7 @@ function update_script() {
   cd ./backend
   $STD pip install -r requirements.txt -U
   cp -rf /opt/open-webui-backup/* /opt/open-webui/backend/data
+  $STD git stash pop
   systemctl start open-webui.service
   msg_ok "Updated Successfully"
   exit
