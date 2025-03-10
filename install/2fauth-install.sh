73907878b6d3ff6fd0eb226a8f3f22f83ad68e26
Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
2025-03-10 13:17:32 +0100

    TEST, DO NOT MERGE

--- install/2fauth-install.sh
+++ install/2fauth-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2025 community-scripts ORG
-# Author: jkrgr0
+# Author: jkr
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://docs.2fauth.app/
 
