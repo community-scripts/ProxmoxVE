c4e51c5675f51bd7a6e19772c42dd363cedf798e
Cerothen <Cerothen@users.noreply.github.com>
2025-03-11 11:39:07 -0400

    Fix Update to enable service

--- ct/headscale.sh
+++ ct/headscale.sh
@@ -41,7 +41,7 @@ function update_script() {
     msg_ok "Updated $APP to ${RELEASE}"
 
     msg_info "Starting ${APP}"
-    systemctl start headscale
+    systemctl enable --now headscale
     msg_ok "Started ${APP}"
     msg_ok "Updated Successfully"
   else
