3acdac369b56d8091331e5430d8dc4ce8ce34745
Scorpoon <22363852+Scorpoon@users.noreply.github.com>
2025-03-17 22:46:23 +0100

    Update trilium-install.sh

--- install/trilium-install.sh
+++ install/trilium-install.sh
@@ -23,9 +23,10 @@ msg_ok "Installed Dependencies"
 msg_info "Setup TriliumNext"
 cd /opt
 RELEASE=$(curl -s https://api.github.com/repos/TriliumNext/Notes/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+EXTRACTED_DIR=$(echo ${RELEASE} | sed 's/^v//')
 wget -q https://github.com/TriliumNext/Notes/releases/download/${RELEASE}/TriliumNextNotes-Server-${RELEASE}-linux-x64.tar.xz
 tar -xf TriliumNextNotes-Server-${RELEASE}-linux-x64.tar.xz
-mv TriliumNextNotes-Server-$RELEASE-linux-x64 /opt/trilium
+mv TriliumNextNotes-Server-${EXTRACTED_DIR}-linux-x64 /opt/trilium
 echo "${RELEASE}" >"/opt/${APPLICATION}_version.txt"
 msg_ok "Setup TriliumNext"
 
