aa087a1e09dc0c2d75d54bca49a11e3feda54168
Slaviša Arežina <arezina.slavisa@gmail.com>
2025-03-21 17:16:52 +0100

    Fix dependency

--- install/gomft-install.sh
+++ install/gomft-install.sh
@@ -35,11 +35,7 @@ ln -sf /usr/local/go/bin/go /usr/local/bin/go
 set -o pipefail
 msg_ok "Setup Golang"
 
-<<<<<<< HEAD
-msg_info "Setup ${APPLICATION}"
-=======
 msg_info "Setup ${APPLICATION} (Patience)"
->>>>>>> 67217457 (Fix dependency)
 temp_file=$(mktemp)
 RELEASE=$(curl -s https://api.github.com/repos/StarFleetCPTN/GoMFT/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
 wget -q "https://github.com/StarFleetCPTN/GoMFT/archive/refs/tags/v${RELEASE}.tar.gz" -O $temp_file
@@ -47,18 +43,11 @@ tar -xzf $temp_file
 mv GoMFT-${RELEASE}/ /opt/gomft
 cd /opt/gomft
 $STD go mod download
-<<<<<<< HEAD
-$STD go build -o gomft
-chmod +x gomft
-$STD go install github.com/a-h/templ/cmd/templ@latest
-$STD $HOME/go/bin/templ generate
-=======
 $STD go install github.com/a-h/templ/cmd/templ@latest
 $STD $HOME/go/bin/templ generate
 export CGO_ENABLED=1
 export GOOS=linux
 $STD go build -o gomft
->>>>>>> 67217457 (Fix dependency)
 JWT_SECRET_KEY=$(openssl rand -base64 24 | tr -d '/+=')
 
 cat <<EOF >/opt/gomft/.env
