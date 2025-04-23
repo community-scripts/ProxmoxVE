e5aa306f5c4c19a7e75e74ed816986cb85db1366
tremor021 <arezina.slavisa@gmail.com>
2025-04-23 16:52:19 +0200

    Revert script changes

--- install/uptimekuma-install.sh
+++ install/uptimekuma-install.sh
@@ -51,7 +51,7 @@ ExecStart=/usr/bin/npm start
 
 [Install]
 WantedBy=multi-user.target" >$service_path
-systemctl enable -q --now uptime-kuma
+$STD systemctl enable --now uptime-kuma
 msg_ok "Created Service"
 
 motd_ssh
