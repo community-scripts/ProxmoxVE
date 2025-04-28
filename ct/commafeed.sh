67541a90fd6e4932613951ccc74d48f5fa59e83c
Maxwell Zhao <max@mzhao.dev>
2025-04-27 21:47:18 -0400

    Install rsync in ct/commafeed.sh
    This commit installs rsync in the update script of commafeed for users who used the install script before rsync was added.

--- ct/commafeed.sh
+++ ct/commafeed.sh
@@ -33,6 +33,10 @@ function update_script() {
     systemctl stop commafeed
     msg_ok "Stopped ${APP}"
 
+    msg_info "Installing Dependencies"
+    $STD apt-get install -y rsync
+    msg_ok "Installed Dependencies"
+
     msg_info "Updating ${APP} to ${RELEASE}"
     curl -fsSL "https://github.com/Athou/commafeed/releases/download/${RELEASE}/commafeed-${RELEASE}-h2-jvm.zip" -o $(basename "https://github.com/Athou/commafeed/releases/download/${RELEASE}/commafeed-${RELEASE}-h2-jvm.zip")
     unzip -q commafeed-"${RELEASE}"-h2-jvm.zip
