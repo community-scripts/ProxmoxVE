3b11450c8c00e1567e115d1a0cc74a3547a118b6
Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
2025-05-14 14:08:43 +0200

    Update asterisk.sh

--- ct/asterisk.sh
+++ ct/asterisk.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-source <(curl -s https://git.community-scripts.org/community-scripts/ProxmoxVED/raw/branch/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: michelroegl-brunner
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://asterisk.org/
 
 APP="Asterisk"
-var_tags="${var_tags:-os}"
+var_tags="${var_tags:-telephone}"
 var_cpu="${var_cpu:-2}"
 var_ram="${var_ram:-2048}"
 var_disk="${var_disk:-4}"
@@ -23,11 +23,7 @@ function update_script() {
     header_info
     check_container_storage
     check_container_resources
-    if [[ ! -d /var ]]; then
-        msg_error "No ${APP} Installation Found!"
-        exit
-    fi
-    msg_info "No Update function provided for ${APP} LXC"
+    msg_error "No Update function provided for ${APP} LXC"
     exit
 }
 
