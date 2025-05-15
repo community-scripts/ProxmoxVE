bbcdc45ee8ce291fdf9fbefd3d89eb6f2b5b9d71
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-05-15 08:56:15 +0200

    Update alpine-bitmagnet.sh

--- ct/alpine-bitmagnet.sh
+++ ct/alpine-bitmagnet.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://git.community-scripts.org/community-scripts/ProxmoxVED/raw/branch/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: Slaviša Arežina (tremor021)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
@@ -26,7 +26,7 @@ function update_script() {
     msg_error "No ${APP} Installation Found!"
     exit 1
   fi
-  RELEASE=$(curl -s https://api.github.com/repos/bitmagnet-io/bitmagnet/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+  RELEASE=$(curl -fsSL https://api.github.com/repos/bitmagnet-io/bitmagnet/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
   if [ "${RELEASE}" != "$(cat /opt/bitmagnet_version.txt)" ] || [ ! -f /opt/bitmagnet_version.txt ]; then
     msg_info "Backing up database"
     rm -f /tmp/backup.sql
