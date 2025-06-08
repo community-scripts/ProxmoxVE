947aa1e928d06d2946c81f29b5ec7796c0be78dc
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-06-08 21:27:32 +0200

    Update minarca.sh

--- ct/minarca.sh
+++ ct/minarca.sh
@@ -6,13 +6,14 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 # Source: https://minarca.org/en_CA
 
 APP="Minarca"
-var_tags="backup"
-var_cpu="2"
-var_ram="4096"
-var_disk="10"
-var_os="debian"
-var_version="12"
-var_unprivileged="1"
+var_tags="${var_tags:-backup}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-4096}"
+var_disk="${var_disk:-10}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-12}"
+var_unprivileged="${var_unprivileged:-1}"
+var_fuse="${var_fuse:-yes}"
 
 header_info "$APP"
 variables
