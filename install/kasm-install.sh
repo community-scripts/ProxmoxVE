665f6833c58d131cce03fa5e9484ab8b96dd8e82
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-06-15 23:33:59 +0200

    Update kasm-install.sh

--- install/kasm-install.sh
+++ install/kasm-install.sh
@@ -25,7 +25,8 @@ motd_ssh
 customize
 
 msg_info "Cleaning up"
-$STD rm -f "/opt/kasm_release_${KASM_VERSION}.tar.gz"
+rm -f "/opt/kasm_release_${KASM_VERSION}.tar.gz"
+rm -f ~/kasm-install.output
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
 msg_ok "Cleaned"
