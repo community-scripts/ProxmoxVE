adb0e88002c2b8d6d57c0ceed559dd8009300408
tremor021 <arezina.slavisa@gmail.com>
2025-06-29 10:29:05 +0200

    Check for directories before backing  up

--- ct/linkwarden.sh
+++ ct/linkwarden.sh
@@ -37,7 +37,7 @@ function update_script() {
 
     msg_info "Updating ${APP} to ${RELEASE}"
     mv /opt/linkwarden/.env /opt/.env
-    mv /opt/linkwarden/data /opt/data.bak
+    [ -d /opt/linkwarden/data ] && mv /opt/linkwarden/data /opt/data.bak
     rm -rf /opt/linkwarden
     fetch_and_deploy_gh_release "linkwarden" "linkwarden/linkwarden"
     cd /opt/linkwarden
@@ -48,7 +48,7 @@ function update_script() {
     $STD yarn prisma:generate
     $STD yarn web:build
     $STD yarn prisma:deploy
-    mv /opt/data.bak /opt/linkwarden/data
+    [ -d /opt/data.bak ] && mv /opt/data.bak /opt/linkwarden/data
     msg_ok "Updated ${APP} to ${RELEASE}"
 
     msg_info "Starting ${APP}"
