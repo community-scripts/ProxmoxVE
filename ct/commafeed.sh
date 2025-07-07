d8a6b29e950c2a9ee13e595c57f9087b1de1126b
tremor021 <arezina.slavisa@gmail.com>
2025-07-07 19:22:25 +0200

    Refactor

--- ct/commafeed.sh
+++ ct/commafeed.sh
@@ -40,11 +40,12 @@ function update_script() {
       $STD apt-get install -y rsync
       msg_ok "Installed Dependencies"
     fi
-
     if [ -d /opt/commafeed/data ] && [ "$(ls -A /opt/commafeed/data)" ]; then
       mv /opt/commafeed/data /opt/data.bak
     fi
     fetch_and_deploy_gh_release "commafeed" "Athou/commafeed" "prebuild" "latest" "/opt/commafeed" "commafeed-*-h2-jvm.zip"
+    d=(/opt/commafeed/commafeed-*/)
+    mv "${d[0]}"* /opt/commafeed/ && rmdir "${d[0]}"
 
     msg_info "Updating ${APP} to ${RELEASE}"
     if [ -d /opt/commafeed/data.bak ] && [ "$(ls -A /opt/commafeed/data.bak)" ]; then
