b7e6366a17583569c409859a031acbb99a5375f7
tremor021 <arezina.slavisa@gmail.com>
2025-07-30 21:12:37 +0200

    Update

--- ct/listmonk.sh
+++ ct/listmonk.sh
@@ -27,6 +27,9 @@ function update_script() {
     msg_error "No ${APP} Installation Found!"
     exit
   fi
+  if ! command -v jq &>/dev/null; then
+    $STD apt-get install -y jq
+  fi
 
   RELEASE=$(curl -fsSL https://api.github.com/repos/knadh/listmonk/releases/latest | jq -r '.tag_name' | sed 's/^v//')
   if [[ "${RELEASE}" != "$(cat ~/.listmonk)" ]] || [[ ! -f ~/.listmonk ]]; then
