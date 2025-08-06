0725f6d82c5a9e85d322b6bb0702c39911877b1e
Andrew Baumbach <andrewb@twingate.com>
2025-08-06 12:21:42 -0400

    chore: bring things over to a branch for testing

--- ct/twingate-connector.sh
+++ ct/twingate-connector.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/twingate-community/ProxmoxVE/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: twingate-andrewb
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
