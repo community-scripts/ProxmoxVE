1370bf63cb856aa94c8e2f7b3f581e867227f2d8
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-08-14 22:21:59 +0200

    Update wordpress-install.sh

--- install/wordpress-install.sh
+++ install/wordpress-install.sh
@@ -13,7 +13,7 @@ setting_up_container
 network_check
 update_os
 
-PHP_VERSION="8.4" PHP_FPM="YES" PHP_MODULE="common,fpm,snmp,imap,mysql" PHP_APACHE="YES" setup_php
+PHP_VERSION="8.4" PHP_FPM="YES" PHP_MODULE="common,snmp,imap,mysql" PHP_APACHE="YES" setup_php
 setup_mariadb
 
 msg_info "Setting up Database"
