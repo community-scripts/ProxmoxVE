8b50f7b0a6f110daa57ccd540d0ebf92ffd01fbf
Ivan Penchev <ivan@penchev.com>
2025-08-25 00:16:49 +0200

    chore: remove my ansible comment

--- tools/pve/post-pve-install.sh
+++ tools/pve/post-pve-install.sh
@@ -524,7 +524,7 @@ if [ -s "$WEB_JS" ] && ! grep -q NoMoreNagging "$WEB_JS"; then
 fi
 
 MOBILE_TPL=/usr/share/pve-yew-mobile-gui/index.html.tpl
-MARKER="<!-- ANSIBLE MANAGED BLOCK FOR MOBILE NAG -->"
+MARKER="<!-- MANAGED BLOCK FOR MOBILE NAG -->"
 if [ -f "$MOBILE_TPL" ] && ! grep -q "$MARKER" "$MOBILE_TPL"; then
     echo "Patching Mobile UI nag..."
     printf "%s\n" \
@@ -533,7 +533,7 @@ if [ -f "$MOBILE_TPL" ] && ! grep -q "$MARKER" "$MOBILE_TPL"; then
       "  function watchAndRemoveDialog() {" \
       "    const observer = new MutationObserver(() => {" \
       "      const dialog = document.querySelector('dialog[aria-label=\"No valid subscription\"]');" \
-      "      if (dialog) { dialog.remove(); console.log('Removed dialog: No valid subscription'); }" \
+      "      if (dialog) { dialog.remove(); console.log('Removed dialog: No valid subscription'); observer.disconnect(); }" \
       "    });" \
       "    observer.observe(document.body, { childList: true, subtree: true });" \
       "  }" \
