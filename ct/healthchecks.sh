81bd9aa15fd20a16b94d72576b2ba29afcecac7a
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-08-25 14:01:14 +0200

    add missing source

--- ct/healthchecks.sh
+++ ct/healthchecks.sh
@@ -3,7 +3,7 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: MickLesk (CanbiZ)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source:
+# Source: https://healthchecks.io/
 
 APP="healthchecks"
 var_tags="${var_tags:-monitoring}"
