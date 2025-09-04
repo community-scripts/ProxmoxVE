18ad968a405785247994b68057e48afc53af13c4
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2025-09-04 23:00:21 +0200

    refactor: Enhance ScriptAccordion and Sidebar components to support selectedCategory state

--- frontend/src/app/scripts/_components/sidebar.tsx
+++ frontend/src/app/scripts/_components/sidebar.tsx
@@ -8,14 +8,18 @@ function Sidebar({
   items,
   selectedScript,
   setSelectedScript,
+  selectedCategory,
+  setSelectedCategory,
 }: {
   items: Category[];
   selectedScript: string | null;
   setSelectedScript: (script: string | null) => void;
+  selectedCategory: string | null;
+  setSelectedCategory: (category: string | null) => void;
 }) {
   const uniqueScripts = items.reduce((acc, category) => {
     for (const script of category.scripts) {
-      if (!acc.some(s => s.name === script.name)) {
+      if (!acc.some((s) => s.name === script.name)) {
         acc.push(script);
       }
     }
@@ -26,17 +30,15 @@ function Sidebar({
     <div className="flex min-w-[350px] flex-col sm:max-w-[350px]">
       <div className="flex items-end justify-between pb-4">
         <h1 className="text-xl font-bold">Categories</h1>
-        <p className="text-xs italic text-muted-foreground">
-          {uniqueScripts.length}
-          {" "}
-          Total scripts
-        </p>
+        <p className="text-xs italic text-muted-foreground">{uniqueScripts.length} Total scripts</p>
       </div>
       <div className="rounded-lg">
         <ScriptAccordion
           items={items}
           selectedScript={selectedScript}
           setSelectedScript={setSelectedScript}
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
         />
       </div>
     </div>
