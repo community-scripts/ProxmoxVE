52bd7c61f0e49477be6d2e046c167a54e070bee0
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-09-08 10:59:34 +0200

    Switch CURL to debian_13 branch

--- ct/adguard.sh
+++ ct/adguard.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/debian_13/misc/build.func)
 # Copyright (c) 2021-2025 tteck
 # Author: tteck (tteckster)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
@@ -20,15 +20,15 @@ color
 catch_errors
 
 function update_script() {
-    header_info
-    check_container_storage
-    check_container_resources
-    if [[ ! -d /opt/AdGuardHome ]]; then
-        msg_error "No ${APP} Installation Found!"
-        exit
-    fi
-    msg_error "Adguard Home should be updated via the user interface."
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -d /opt/AdGuardHome ]]; then
+    msg_error "No ${APP} Installation Found!"
     exit
+  fi
+  msg_error "Adguard Home should be updated via the user interface."
+  exit
 }
 
 start
