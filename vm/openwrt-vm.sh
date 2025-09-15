722ee1216804c3277a17e7696e1e163a96862ac7
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-09-15 10:54:52 +0200

    move openwrt to correct naming

--- vm/openwrt-vm.sh
+++ vm/openwrt-vm.sh
@@ -28,7 +28,7 @@ METHOD=""
 NSAPP="openwrt-vm"
 var_os="openwrt"
 var_version=" "
-DISK_SIZE="0.5G"
+DISK_SIZE="1G"
 GEN_MAC=02:$(openssl rand -hex 5 | awk '{print toupper($0)}' | sed 's/\(..\)/\1:/g; s/.$//')
 GEN_MAC_LAN=02:$(openssl rand -hex 5 | awk '{print toupper($0)}' | sed 's/\(..\)/\1:/g; s/.$//')
 
