76726753aa0e191e46692aa02ad8503974e79178
tremor021 <arezina.slavisa@gmail.com>
2025-09-17 09:20:50 +0200

    Refactor

--- install/grist-install.sh
+++ install/grist-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2025 community-scripts ORG
-# Author: cfurrow
+# Author: cfurrow | Co-Author: Slaviša Arežina (tremor021)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/gristlabs/grist-core
 
@@ -16,21 +16,21 @@ update_os
 msg_info "Installing Dependencies"
 $STD apt-get install -y \
   make \
-  ca-certificates \
-  python3.11-venv
+  ca-certificates
 msg_ok "Installed Dependencies"
 
+if grep -q '^13' /etc/debian_version; then
+  $STD apt-get install -y python3.13-venv
+else
+  $STD apt-get install -y python3.11-venv
+fi
 NODE_VERSION="22" NODE_MODULE="yarn@latest" setup_nodejs
+fetch_and_deploy_gh_release "grist" "gristlabs/grist-core" "tarball"
 
 msg_info "Installing Grist"
-RELEASE=$(curl -fsSL https://api.github.com/repos/gristlabs/grist-core/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
 export CYPRESS_INSTALL_BINARY=0
 export NODE_OPTIONS="--max-old-space-size=2048"
-cd /opt
-curl -fsSL "https://github.com/gristlabs/grist-core/archive/refs/tags/v${RELEASE}.zip" -o "v${RELEASE}.zip"
-$STD unzip v$RELEASE.zip
-mv grist-core-${RELEASE} grist
-cd grist
+cd /opt/grist
 $STD yarn install
 $STD yarn run build:prod
 $STD yarn run install:python
@@ -64,7 +64,6 @@ motd_ssh
 customize
 
 msg_info "Cleaning up"
-rm -rf /opt/v${RELEASE}.zip
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
 msg_ok "Cleaned"
