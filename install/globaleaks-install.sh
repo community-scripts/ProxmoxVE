83b887e75e3fd3b50f230bb9de42717092d62265
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-09-18 12:18:21 +0200

    Update globaleaks-install.sh

--- install/globaleaks-install.sh
+++ install/globaleaks-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Copyright (c) 2021-2025 community-scripts ORG
-# Author: Giovanni `evilaliv3` Pellerano
+# Author: Giovanni Pellerano (evilaliv3)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/globaleaks/globaleaks-whistleblowing-software
 
