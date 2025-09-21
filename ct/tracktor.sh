8fdd860bb089f2be2ee0114b19536bb31cc3670e
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-09-21 11:09:13 +0200

    tracktor: add: cleanup before upgrade

--- ct/tracktor.sh
+++ ct/tracktor.sh
@@ -38,6 +38,7 @@ function update_script() {
     msg_ok "Created Backup"
 
     setup_nodejs
+    rm -rf /opt/tracktor && sleep 2
     fetch_and_deploy_gh_release "tracktor" "javedh-dev/tracktor" "tarball" "latest" "/opt/tracktor"
 
     msg_info "Updating ${APP}"
