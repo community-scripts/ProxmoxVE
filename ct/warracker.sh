b1a49fb7901adc933a28bf2eeadcd1dcbd9fe6f4
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-09-29 13:52:20 +0200

    Correct application name casing in warracker.sh

--- ct/warracker.sh
+++ ct/warracker.sh
@@ -5,7 +5,7 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/sassanix/Warracker/
 
-APP="warracker"
+APP="Warracker"
 var_tags="${var_tags:-warranty}"
 var_cpu="${var_cpu:-1}"
 var_ram="${var_ram:-512}"
