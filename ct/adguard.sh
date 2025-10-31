18edfc2473937da01688f8e868d2c3e2efb5ad7c
Gerhard Burger <burger.ga@gmail.com>
2025-10-31 11:29:47 +0100

    Add real Adguard Home update

--- ct/adguard.sh
+++ ct/adguard.sh
@@ -27,7 +27,39 @@ function update_script() {
     msg_error "No ${APP} Installation Found!"
     exit
   fi
-  msg_error "Adguard Home can only be updated via the user interface."
+  if check_for_gh_release "AdGuardHome" "AdguardTeam/AdGuardHome"; then
+    read -r -p "It is recommended to update AdGuard Home from the web interface. Would you like to continue with a manual update? <y/N> " prompt
+    if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
+      # After stopping Adguard Home service, github.com might not resolve anymore -> first install into temp location
+      msg_info "Installing AdGuard Home to temporary location"
+      fetch_and_deploy_gh_release "AdGuardHome" "AdguardTeam/AdGuardHome" "prebuild" "latest" "/opt/AdGuardHome.temp" "AdGuardHome_linux_amd64.tar.gz"
+      msg_ok "Installed AdGuard Home to temporary location"
+
+      msg_info "Stopping Service"
+      systemctl stop AdGuardHome
+      msg_ok "Stopped Service"
+
+      msg_info "Backing up Configuration"
+      cp /opt/AdGuardHome/AdGuardHome.yaml /opt/AdGuardHome.yaml
+      cp -r /opt/AdGuardHome/data /opt/AdGuardHome_data
+      msg_ok "Backed up Configuration"
+
+      msg_info "Moving new AdGuard Home to correct location"
+      rm -rf /opt/AdGuardHome
+      mv /opt/AdGuardHome.temp /opt/AdGuardHome
+      msg_ok "Moved new AdGuard Home to correct location"
+
+      msg_info "Restoring Configuration"
+      mv /opt/AdGuardHome.yaml /opt/AdGuardHome/AdGuardHome.yaml
+      rm -rf /opt/AdGuardHome/data
+      mv /opt/AdGuardHome_data /opt/AdGuardHome/data
+      msg_ok "Restored Configuration"
+      
+      msg_info "Starting Service"
+      systemctl start AdGuardHome
+      msg_ok "Started Service"
+    fi
+  fi
   exit
 }
 
