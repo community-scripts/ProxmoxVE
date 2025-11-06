9179cc8e5828f5673e098ea3cc30e2c089611529
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-11-06 22:13:55 +0100

    Update Infisical installation script with dynamic keys

--- install/infisical-install.sh
+++ install/infisical-install.sh
@@ -51,12 +51,14 @@ $STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET timezone TO 'UTC';"
 msg_ok "Configured PostgreSQL"
 
 msg_info "Setting up Infisical"
+AUTH_SECRET="$(openssl rand -base64 32 | tr -d '\n')"
+ENC_KEY="$(openssl rand -hex 16 | tr -d '\n')"
 IP_ADDR=$(hostname -I | awk '{print $1}')
 $STD apt install -y infisical-core
 mkdir -p /etc/infisical
 cat <<EOF >/etc/infisical/infisical.rb
-infisical_core['ENCRYPTION_KEY'] = '6c1fe4e407b8911c104518103505b218'
-infisical_core['AUTH_SECRET'] = '5lrMXKKWCVocS/uerPsl7V+TX/aaUaI7iDkgl3tSmLE='
+infisical_core['ENCRYPTION_KEY'] = '$ENC_KEY'
+infisical_core['AUTH_SECRET'] = '$AUTH_SECRET'
 infisical_core['HOST'] = '$IP_ADDR'
 infisical_core['DB_CONNECTION_URI'] = 'postgres://${DB_USER}:${DB_PASS}@localhost:5432/${DB_NAME}'
 infisical_core['REDIS_URL'] = 'redis://localhost:6379'
