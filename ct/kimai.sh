d0595c911f5dcc6e1b68756be9e528fa855ba6f6
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-11-06 08:24:32 -0800

    fix kimai Update Check

--- ct/kimai.sh
+++ ct/kimai.sh
@@ -34,7 +34,7 @@ function update_script() {
   PHP_VERSION="8.4" PHP_MODULE="mysql" PHP_APACHE="YES" setup_php
   setup_composer
 
-  if check_for_gh_release "koillection" "benjaminjonard/koillection"; then
+    if check_for_gh_release "kimai" "kimai/kimai"; then
     BACKUP_DIR="/opt/kimai_backup"
 
     msg_info "Stopping Apache2"
