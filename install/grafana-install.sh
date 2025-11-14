c522f0e5c1dc4e829787e345f8a5d27d88226e60
Matthias Schabhüttl <matthias@matthiasschabhuettl.com>
2025-11-14 09:43:07 +0100

    Changed to new func setup_deb822_repo

--- install/grafana-install.sh
+++ install/grafana-install.sh
@@ -14,25 +14,29 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y \
+$STD apt install -y \
   apt-transport-https
 msg_ok "Installed Dependencies"
 
 msg_info "Setting up Grafana Repository"
-curl -fsSL "https://apt.grafana.com/gpg.key" -o "/usr/share/keyrings/grafana.key"
-echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" >/etc/apt/sources.list.d/grafana.list
-msg_ok "Set up Grafana Repository"
+setup_deb822_repo \
+  "grafana" \
+  "https://apt.grafana.com/gpg.key" \
+  "https://apt.grafana.com" \
+  "stable" \
+  "main"
+$STD apt update
+msg_ok "Grafana Repository setup sucessfully"
 
 msg_info "Installing Grafana"
-$STD apt-get update
-$STD apt-get install -y grafana
-systemctl enable -q --now  grafana-server
+$STD apt install -y grafana
+systemctl enable -q --now grafana-server
 msg_ok "Installed Grafana"
 
 motd_ssh
 customize
 
 msg_info "Cleaning up"
-$STD apt-get -y autoremove
-$STD apt-get -y autoclean
+$STD apt -y autoremove
+$STD apt -y autoclean
 msg_ok "Cleaned"
