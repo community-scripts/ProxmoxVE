09e0e06cca53f222a5b68cf03982cbeb85e18df4
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-12-14 17:11:26 +0100

    Update homarr-install.sh

--- install/homarr-install.sh
+++ install/homarr-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2025 community-scripts ORG
-# Author: CrazyWolf13
+# Author: MickLesk (CanbiZ) | Co-Author: CrazyWolf13
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/homarr-labs/homarr
 
