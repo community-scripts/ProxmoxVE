0ed8bc4d74e81baab856464214a16e272252ae6a
Karim SAD <karim.sad@sap.com>
2025-12-25 14:08:29 -0500

    Add 'DAEMON_OPTS' variable to allow passing more arguments to the server

--- install/duplicati-install.sh
+++ install/duplicati-install.sh
@@ -38,7 +38,8 @@ Description=Duplicati Service
 After=network.target
 
 [Service]
-ExecStart=/usr/bin/duplicati-server --webservice-interface=any --webservice-password=$ADMINPASS --settings-encryption-key=$DECRYPTKEY
+EnvironmentFile=-/etc/default/duplicati
+ExecStart=/usr/bin/duplicati-server --webservice-interface=any --webservice-password=$ADMINPASS --settings-encryption-key=$DECRYPTKEY $DAEMON_OPTS
 Restart=always
 
 [Install]
