65b5c10d8c17d75278f862e6ce78f5e003552095
Tobias <git@tobiasmeier.dev>
2026-01-06 16:50:16 +0100

    fix: tteck co-author

--- tools/pve/pbs_microcode.sh
+++ tools/pve/pbs_microcode.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2026 community-scripts ORG
-# Author: DonPablo1010
+# Author: DonPablo1010 | Co-Author: tteck (tteckster)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 
 function header_info {
