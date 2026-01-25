87c020be31de3f1801a73e9b4c6010b261a23c41
MickLesk <mickey.leskowitz@gmail.com>
2026-01-25 08:54:01 +0100

    remove spaces

--- ct/termix.sh
+++ ct/termix.sh
@@ -5,7 +5,7 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/Termix-SSH/Termix
 
-APP="Termix"  
+APP="Termix"
 var_tags="${var_tags:-ssh;terminal;management}"
 var_cpu="${var_cpu:-4}"
 var_ram="${var_ram:-4096}"
