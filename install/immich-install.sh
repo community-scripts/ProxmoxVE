c47fa83a575a7a7100e2aabcadaf23d6b1f514ea
vhsdream <punk.sand7393@fastmail.com>
2026-01-31 18:39:25 -0500

    Refactor service healthchecks

--- install/immich-install.sh
+++ install/immich-install.sh
@@ -479,22 +479,23 @@ msg_ok "Modified user, created env file, scripts and services"
 
 msg_info "Checking health of Immich-web & Immich-ml services"
 sleep 5
-if ! curl -fs localhost:2283/api/server/ping | grep -q "pong"; then
+web=0
+until curl -fs localhost:2283/api/server/ping | grep -q "pong" || [[ $web -gt 1 ]]; do
   msg_warn "Problem detected with Immich-web service, restarting..."
   systemctl restart immich-web && sleep 5
-  [[ ! $(curl -fs localhost:2283/api/server/ping | grep "pong") ]] && msg_error "Please check '/var/log/immich/web.log' for more details"
-  msg_ok "Immich-web service is reachable!"
-else
-  msg_ok "Immich-web service is reachable!"
-fi
-if [[ $(curl -fs localhost:3003/ping) != "pong" ]]; then
+  [[ $(curl -fs localhost:2283/api/server/ping | grep "pong") ]] && break
+  ((web++))
+done
+[[ $web -lt 1 ]] && msg_ok "Immich-web service is reachable!" || msg_error "Please check '/var/log/immich/web.log' for more details"
+
+ml=0
+until [[ $(curl -fs localhost:3003/ping) == "pong" ]] || [[ $ml -gt 1 ]]; do
   msg_warn "Problem detected with Immich-ml service, restarting..."
   systemctl restart immich-ml && sleep 5
-  [[ $(curl -fs localhost:3003/ping) != "pong" ]] && msg_error "Please check '/var/log/immich/ml.log' for more details"
-  msg_ok "Immich-ml service is reachable!"
-else
-  msg_ok "Immich-ml service is reachable!"
-fi
+  [[ $(curl -fs localhost:3003/ping) == "pong" ]] && break
+  ((ml++))
+done
+[[ $ml -lt 1 ]] && msg_ok "Immich-ml service is reachable!" || msg_error "Please check '/var/log/immich/ml.log' for more details"
 
 motd_ssh
 customize
