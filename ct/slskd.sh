e0641a13772be5a09828fa059402fcbc36d660d1
vhsdream <punk.sand7393@fastmail.com>
2026-02-10 14:12:31 -0500

    Update Soularr install & update

--- ct/slskd.sh
+++ ct/slskd.sh
@@ -3,7 +3,7 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: vhsdream
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://github.com/slskd/slskd
+# Source: https://github.com/slskd/slskd, https://github.com/mrusse/soularr
 
 APP="slskd"
 var_tags="${var_tags:-arr;p2p}"
@@ -49,28 +49,40 @@ function update_script() {
     systemctl start slskd
     [[ -f /etc/systemd/system/soularr.service ]] && systemctl start soularr.timer
     msg_ok "Started Service(s)"
-    msg_ok "Updated successfully!"
+    msg_ok "Updated Slskd successfully!"
   fi
-  # [[ -d /opt/soularr ]] && if check_for_gh_release "Soularr" "mrusse/soularr"; then
-  #   if systemctl is-active soularr.timer; then
-  #     msg_info "Stopping Timer and Service"
-  #     systemctl stop soularr.timer soularr.service
-  #     msg_ok "Stopped Timer and Service"
-  #   fi
-  #
-  #   msg_info "Backing up Soularr config"
-  #   cp /opt/soularr/config.ini /opt/soularr_config.ini.bak
-  #   cp /opt/soularr/run.sh /opt/soularr_run.sh.bak
-  #   $STD pip install -r requirements.txt
-  #   mv /opt/config.ini.bak /opt/soularr/config.ini
-  #   mv /opt/run.sh.bak /opt/soularr/run.sh
-  #   rm -rf /tmp/main.zip
-  #   msg_ok "Updated soularr"
-  #
-  #   msg_info "Starting soularr timer"
-  #   systemctl start soularr.timer
-  #   msg_ok "Started soularr timer"
-  # fi
+  [[ -d /opt/soularr ]] && if check_for_gh_release "Soularr" "mrusse/soularr"; then
+    if systemctl is-active soularr.timer >/dev/null; then
+      msg_info "Stopping Timer and Service"
+      systemctl stop soularr.timer soularr.service
+      msg_ok "Stopped Timer and Service"
+    fi
+
+    msg_info "Backing up Soularr config"
+    cp /opt/soularr/config.ini /opt/soularr_config.ini.bak
+    cp /opt/soularr/run.sh /opt/soularr_run.sh.bak
+    msg_ok "Backed up Soularr config"
+
+    PYTHON_VERSION="3.11" setup_uv
+    CLEAN_INSTALL=1 fetch_and_deploy_gh_release "Soularr" "mrusse/soularr" "tarball" "latest" "/opt/soularr"
+    msg_info "Updating Soularr"
+    cd /opt/soularr
+    $STD uv venv -c venv
+    $STD source venv/bin/activate
+    $STD uv pip install -r requirements.txt
+    deactivate
+    msg_ok "Updated Soularr"
+
+    msg_info "Restoring Soularr config"
+    mv /opt/soularr_config.ini.bak /opt/soularr/config.ini
+    mv /opt/soularr_run.sh.bak /opt/soularr/run.sh
+    msg_ok "Restored Soularr config"
+
+    msg_info "Starting Soularr Timer"
+    systemctl restart soularr.timer
+    msg_ok "Started Soularr Timer"
+    msg_ok "Updated Soularr successfully!"
+  fi
 }
 
 start
--- install/slskd-install.sh
+++ install/slskd-install.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: vhsdream
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://github.com/slskd/slskd/
+# Source: https://github.com/slskd/slskd/, https://github.com/mrusse/soularr
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
 color
@@ -32,27 +32,33 @@ sed -i \
   -e 's/^.*picture/#&/' /opt/slskd/config/slskd.yml
 msg_ok "Configured Slskd"
 
-# read -rp "${TAB3}Do you want to install Soularr? y/N " soularr
-# if [[ ${soularr,,} =~ ^(y|yes)$ ]]; then
-#   msg_info "Installing Dependencies"
-#   $STD apt install -y python3-pip
-#   msg_ok "Installed Dependencies"
-#   cd /opt/soularr
-#   $STD pip install -r requirements.txt
-#   sed -i \
-#     -e "\|[Slskd]|,\|host_url|s|yourslskdapikeygoeshere|$SLSKD_API_KEY|" \
-#     -e "/host_url/s/slskd/localhost/" \
-#     /opt/soularr/config.ini
-#   sed -i \
-#     -e "/#This\|#Default\|INTERVAL/{N;d;}" \
-#     -e "/while\|#Pass/d" \
-#     -e "\|python|s|app|opt/soularr|; s|python|python3|" \
-#     -e "/dt/,+2d" \
-#     /opt/soularr/run.sh
-#   sed -i -E "/(soularr.py)/s/.{5}$//; /if/,/fi/s/.{4}//" /opt/soularr/run.sh
-#   chmod +x /opt/soularr/run.sh
-#   msg_ok "Installed Soularr"
-# fi
+read -rp "${TAB3}Do you want to install Soularr? y/N " soularr
+if [[ ${soularr,,} =~ ^(y|yes)$ ]]; then
+  PYTHON_VERSION="3.11" setup_uv
+  fetch_and_deploy_gh_release "Soularr" "mrusse/soularr" "tarball" "latest" "/opt/soularr"
+  cd /opt/soularr
+  $STD uv venv venv
+  $STD source venv/bin/activate
+  $STD uv pip install -r requirements.txt
+  sed -i \
+    -e "\|[Slskd]|,\|host_url|s|yourslskdapikeygoeshere|$SLSKD_API_KEY|" \
+    -e "/host_url/s/slskd/localhost/" \
+    /opt/soularr/config.ini
+  cat <<EOF >/opt/soularr/run.sh
+#!/usr/bin/env bash
+
+if ps aux | grep "[s]oularr.py" >/dev/null; then
+  echo "Soularr is already running. Exiting..."
+  exit 1
+else
+  source /opt/soularr/venv/bin/activate
+  uv run python3 -u /opt/soularr/soularr.py --config-dir /opt/soularr
+fi
+EOF
+  chmod +x /opt/soularr/run.sh
+  deactivate
+  msg_ok "Installed Soularr"
+fi
 
 msg_info "Creating Service"
 cat <<EOF >/etc/systemd/system/slskd.service
@@ -70,38 +76,38 @@ Restart=always
 WantedBy=multi-user.target
 EOF
 
-# if [[ -d /opt/soularr ]]; then
-#   cat <<EOF >/etc/systemd/system/soularr.timer
-# [Unit]
-# Description=Soularr service timer
-# RefuseManualStart=no
-# RefuseManualStop=no
-#
-# [Timer]
-# Persistent=true
-# # run every 5 minutes
-# OnCalendar=*-*-* *:0/5:00
-# Unit=soularr.service
-#
-# [Install]
-# WantedBy=timers.target
-# EOF
-#
-#   cat <<EOF >/etc/systemd/system/soularr.service
-# [Unit]
-# Description=Soularr service
-# After=network.target slskd.service
-#
-# [Service]
-# Type=simple
-# WorkingDirectory=/opt/soularr
-# ExecStart=/bin/bash -c /opt/soularr/run.sh
-#
-# [Install]
-# WantedBy=multi-user.target
-# EOF
-#   msg_warn "Add your Lidarr API key to Soularr in '/opt/soularr/config.ini', then run 'systemctl enable --now soularr.timer'"
-# fi
+if [[ -d /opt/soularr ]]; then
+  cat <<EOF >/etc/systemd/system/soularr.timer
+[Unit]
+Description=Soularr service timer
+RefuseManualStart=no
+RefuseManualStop=no
+
+[Timer]
+Persistent=true
+# run every 10 minutes
+OnCalendar=*-*-* *:0/10:00
+Unit=soularr.service
+
+[Install]
+WantedBy=timers.target
+EOF
+
+  cat <<EOF >/etc/systemd/system/soularr.service
+[Unit]
+Description=Soularr service
+After=network.target slskd.service
+
+[Service]
+Type=simple
+WorkingDirectory=/opt/soularr
+ExecStart=/bin/bash -c /opt/soularr/run.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+  msg_warn "Add your Lidarr API key to Soularr in '/opt/soularr/config.ini', then run 'systemctl enable --now soularr.timer'"
+fi
 systemctl enable -q --now slskd
 msg_ok "Created Services"
 
