0badfeb54aca227dfe9b643c79b624716030f54b
Gian Marco Cinalli <gm.cinalli@gmail.com>
2026-02-19 12:25:17 +0100

    fix: update plex repository and key

--- install/plex-install.sh
+++ install/plex-install.sh
@@ -16,13 +16,13 @@ update_os
 setup_hwaccel
 
 msg_info "Setting Up Plex Media Server Repository"
-curl -fsSL https://downloads.plex.tv/plex-keys/PlexSign.key | tee /usr/share/keyrings/PlexSign.asc >/dev/null
+curl -fsSL https://downloads.plex.tv/plex-keys/PlexSign.v2.key | gpg --yes --dearmor | tee /etc/apt/keyrings/plexmediaserver.v2.gpg > /dev/null
 cat <<EOF >/etc/apt/sources.list.d/plexmediaserver.sources
 Types: deb
-URIs: https://downloads.plex.tv/repo/deb/
+URIs: https://repo.plex.tv/deb/
 Suites: public
 Components: main
-Signed-By: /usr/share/keyrings/PlexSign.asc
+Signed-By: /etc/apt/keyrings/plexmediaserver.v2.gpg
 EOF
 msg_ok "Set Up Plex Media Server Repository"
 
