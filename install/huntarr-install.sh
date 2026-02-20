674ab93068280499b0c2a1620c8428928dc11314
CanbiZ (MickLesk) <47820557+MickLesk@users.noreply.github.com>
2026-02-20 21:19:30 +0100

    Change apt-get to apt for installing dependencies

--- install/huntarr-install.sh
+++ install/huntarr-install.sh
@@ -14,7 +14,7 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y build-essential
+$STD apt install -y build-essential
 msg_ok "Installed Dependencies"
 
 PYTHON_VERSION="3.12" setup_uv
