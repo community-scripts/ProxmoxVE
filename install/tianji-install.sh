a594c1ab286fe3a8ddd464b4c689120c3da20a69
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2026-03-04 20:30:12 +0100

    Update tianji-install.sh

--- install/tianji-install.sh
+++ install/tianji-install.sh
@@ -27,6 +27,7 @@ PG_VERSION="17" setup_postgresql
 PG_DB_NAME="tianji_db" PG_DB_USER="tianji" setup_postgresql_db
 PYTHON_VERSION="3.13" setup_uv
 fetch_and_deploy_gh_release "tianji" "msgbyte/tianji" "tarball"
+TIANJI_SECRET="$(openssl rand -base64 32 | cut -c1-24)"
 
 msg_info "Setting up Tianji"
 cd /opt/tianji
