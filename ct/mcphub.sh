a381ecdd53df8b7d0515dc3ceff538753b10724a
John Doe <johndoe@example.com>
2026-03-08 19:03:16 -0400

    New Url Variable for build.func

--- ct/mcphub.sh
+++ ct/mcphub.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-unscripted/ProxmoxVED/main/misc/build.func)
+COMMUNITY_SCRIPTS_URL="${COMMUNITY_SCRIPTS_URL:-https://raw.githubusercontent.com/community-unscripted/ProxmoxVE/refs/heads/main}"
+source <(curl -fsSL ${COMMUNITY_SCRIPTS_URL}/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: BillyOutlast
 # License: MIT | https://github.com/community-unscripted/ProxmoxVE/raw/main/LICENSE
--- ct/swarmui.sh
+++ ct/swarmui.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-unscripted/ProxmoxVED/main/misc/build.func)
+COMMUNITY_SCRIPTS_URL="${COMMUNITY_SCRIPTS_URL:-https://raw.githubusercontent.com/community-unscripted/ProxmoxVE/refs/heads/main}"
+source <(curl -fsSL ${COMMUNITY_SCRIPTS_URL}/misc/build.func)
 
 # Copyright (c) 2021-2026 community-scripts ORG
-# Author: community-scripts
+# Author: BillyOutlast
 # License: MIT | https://github.com/community-unscripted/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/mcmonkeyprojects/SwarmUI
 
