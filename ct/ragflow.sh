38cff929b78b206a35775f0515765408f96d70bd
John Doe <johndoe@example.com>
2026-03-19 19:30:03 -0400

    remove copyright

--- ct/ragflow.sh
+++ ct/ragflow.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 COMMUNITY_SCRIPTS_URL="${COMMUNITY_SCRIPTS_URL:-https://raw.githubusercontent.com/Heretek-AI/ProxmoxVE/refs/heads/main}"
 source <(curl -fsSL "${COMMUNITY_SCRIPTS_URL}"/misc/build.func)
-# Copyright (c) 2021-2026 community-scripts ORG
 # Author: BillyOutlast
 # License: MIT | https://github.com/Heretek-AI/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/infiniflow/ragflow
