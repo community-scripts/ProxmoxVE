52a6f7718f04be2cb35cebd97e10533da8430a1f
Aziz FADIL <aziz.fadil@gmail.com>
2026-04-03 09:03:43 -0400

    Unpin release version to fetch latest

--- install/wealthfolio-install.sh
+++ install/wealthfolio-install.sh
@@ -24,7 +24,7 @@ msg_ok "Installed Dependencies"
 
 setup_rust
 NODE_VERSION="24" NODE_MODULE="pnpm" setup_nodejs
-fetch_and_deploy_gh_release "wealthfolio" "afadil/wealthfolio" "tarball" "v3.2.1"
+fetch_and_deploy_gh_release "wealthfolio" "afadil/wealthfolio" "tarball"
 
 msg_info "Building Frontend (patience)"
 cd /opt/wealthfolio
