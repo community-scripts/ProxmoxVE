84c3699f309e0d0ebbcba76eab6cf7769d5c71ee
Tom Frenzel <admin@tom-frenzel.de>
2026-06-03 20:32:38 +0200

    chore(openthread-br): add tomfrenzel as co-author

--- ct/openthread-br.sh
+++ ct/openthread-br.sh
@@ -3,6 +3,7 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: MickLesk (CanbiZ)
+# Co-Author: Tom Frenzel (tomfrenzel)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://openthread.io/guides/border-router
 
