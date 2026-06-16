55e01b22d8c5d459bb10dfa92c5df4c9e1782d70
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2026-06-16 22:20:50 +0200

    Change author to CrazyWolf13 in kasm-install.sh
    Updated author information in the script header.

--- install/kasm-install.sh
+++ install/kasm-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2026 community-scripts ORG
-# Author: Omar Minaya | Co-Author: CrazyWolf13
+# Author: CrazyWolf13
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://www.kasmweb.com/docs/latest/index.html
 
