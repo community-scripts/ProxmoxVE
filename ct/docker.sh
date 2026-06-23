0dec2f371038f1354a11549700780da37b3581db
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2026-06-24 00:12:12 +1000

    fix docker update function

--- ct/docker.sh
+++ ct/docker.sh
@@ -27,12 +27,10 @@ function update_script() {
 
   msg_info "Updating base system"
   $STD apt update
-  $STD apt upgrade -y 
+  $STD apt upgrade -y
   msg_ok "Base system updated"
 
-  msg_info "Updating Docker Engine"
-  $STD apt install --only-upgrade -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-buildx-plugin
-  msg_ok "Docker Engine updated"
+  setup_docker
 
   if docker ps -a --format '{{.Image}}' | grep -q '^portainer/portainer-ce:latest$'; then
     msg_info "Updating Portainer"
