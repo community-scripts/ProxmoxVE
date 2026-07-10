d0a22df6709673c6291f6c8e3e1a294dd259a000
CanbiZ (MickLesk) <47820557+MickLesk@users.noreply.github.com>
2026-07-10 15:42:21 +0200

    Change default var_arm64 value from 'no' to 'yes'

--- ct/skylite-ux.sh
+++ ct/skylite-ux.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
-
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: bzumhagen
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
@@ -13,7 +12,7 @@ var_ram="${var_ram:-4096}"
 var_disk="${var_disk:-8}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-13}"
-var_arm64="${var_arm64:-no}"
+var_arm64="${var_arm64:-yes}"
 var_unprivileged="${var_unprivileged:-1}"
 
 header_info "$APP"
