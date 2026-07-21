3200758cfb51ee79ab182509ad6b1ec63d3abb00
MickLesk <mickey.leskowitz@gmail.com>
2026-07-21 13:11:40 +0200

    fix url for image

--- vm/opnsense-vm.sh
+++ vm/opnsense-vm.sh
@@ -772,7 +772,7 @@ DESCRIPTION=$(
   cat <<EOF
 <div align='center'>
   <a href='https://community-scripts.org' target='_blank' rel='noopener noreferrer'>
-    <img src='https://raw.githubusercontent.com/michelroegl-brunner/ProxmoxVE/refs/heads/develop/misc/images/logo-81x112.png' alt='Logo' style='width:81px;height:112px;'/>
+    <img src='https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/images/logo-81x112.png' alt='Logo' style='width:81px;height:112px;'/>
   </a>
 
   <h2 style='font-size: 24px; margin: 20px 0;'>OPNsense VM</h2>
