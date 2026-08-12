dff6cdab2364dce26970fb700caaac87a605f051
Johann Grobe <j.grobe@namowo.de>
2026-08-12 12:23:51 +0200

    fix migration check

--- ct/wanderer.sh
+++ ct/wanderer.sh
@@ -30,7 +30,7 @@ function update_script() {
     exit
   fi
 
-  if [[ -f /opt/wanderer/source/search/meilisearch ]]; then
+  if [[ -d /opt/wanderer/source/search ]]; then
     msg_info "Migrating Meilisearch"
     systemctl stop wanderer-web 2>/dev/null
 
