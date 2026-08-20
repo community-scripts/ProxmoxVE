19a625a36a0a7d0d50eb62ecbc1fc69a3c9055f0
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2026-08-20 10:01:25 +0200

    fix: author

--- ct/umbraco.sh
+++ ct/umbraco.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
-# Author: Joost van den Berg
+# Author: Joost van den Berg (montagneId)
 # License: MIT | https://github.com/montagneid/ProxmoxVED/raw/main/LICENSE
 # Source: https://github.com/umbraco/Umbraco-CMS
 
