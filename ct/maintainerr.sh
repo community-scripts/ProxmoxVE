b2a1b302839b4d70869b0a296c65d9db13c78bbe
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2026-08-24 16:51:36 +0200

    add: author

--- ct/maintainerr.sh
+++ ct/maintainerr.sh
@@ -3,7 +3,7 @@ _CS_DEFAULT_URL="https://raw.githubusercontent.com/community-scripts/ProxmoxVE/m
 _cs_boot="${COMMUNITY_SCRIPTS_CORE_DIR:-$(dirname "${BASH_SOURCE[0]}")/../../core}/core/build.func"
 source "$_cs_boot" 2>/dev/null || source <(curl -fsSL "${COMMUNITY_SCRIPTS_CORE_URL:-https://raw.githubusercontent.com/community-scripts/core/main}/core/build.func")
 # Copyright (c) 2021-2026 community-scripts ORG
-# Author: Karolis Stanelis
+# Author: Karolis Stanelis (kstanelis)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/Maintainerr/Maintainerr
 
