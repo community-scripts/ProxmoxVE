d0927b8013a42974f2957952765a8ac6d897e736
Sam Heinz <sam@samheinz.com>
2026-08-26 21:31:49 +1000

    Update ct/gentoo.sh

--- ct/gentoo.sh
+++ ct/gentoo.sh
@@ -43,4 +43,4 @@ build_container
 description
 
 msg_ok "Completed successfully!"
-msg_custom "🚀" "${GN}" "${APP} setup has been successfully initialized!"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
