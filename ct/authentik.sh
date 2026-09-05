277a7a585095f43793b7d7b264e54df3ba968e95
CanbiZ (MickLesk) <47820557+MickLesk@users.noreply.github.com>
2026-09-05 12:28:14 +0200

    Update ct/authentik.sh
    Co-authored-by: thieneret <123479547+thieneret@users.noreply.github.com>

--- ct/authentik.sh
+++ ct/authentik.sh
@@ -179,7 +179,7 @@ EOF
 
     msg_info "Updating Worker and Server config"
     sed -i "s|/dev/shm$|/dev/shm/authentik-server|g" /etc/default/authentik-server
-	  sed -i "s|/dev/shm$|/dev/shm/authentik-worker|g" /etc/default/authentik-worker
+	  sed -i "s|/dev/shm\/\?$|/dev/shm/authentik-worker|g" /etc/default/authentik-worker
     msg_ok "Updated Worker and Server config"
 
     msg_info "Updating services"
