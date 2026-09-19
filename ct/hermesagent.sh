7e0c2dc94b53e3368d33e94efa5514c5a654c345
Jintak Han <jintakhanphoto@gmail.com>
2026-09-20 05:03:49 +0900

    Add service migration logic to update script

--- ct/hermesagent.sh
+++ ct/hermesagent.sh
@@ -43,9 +43,23 @@ function update_script() {
     exit 10
   fi
 
-  msg_info "Stopping Services"
-  su - hermes -c 'systemctl --user stop hermes-dashboard'
-  msg_ok "Stopped Services"
+  if [ -f /etc/systemd/system/hermes-dashboard.service ]; then
+    msg_info "Migrating Services"
+    systemctl stop hermes-dashboard
+    systemctl disable hermes-dashboard
+    mkdir -p /home/hermes/.config/systemd/user
+    mv /etc/systemd/system/hermes-dashboard.service* /home/hermes/.config/systemd/user/
+    sed -i '/User=hermes/d' /home/hermes/.config/systemd/user/hermes-dashboard.service
+    sed -i '/Group=hermes/d' /home/hermes/.config/systemd/user/hermes-dashboard.service
+    sed -i 's/WantedBy=multi-user.target/WantedBy=default.target/g' /home/hermes/.config/systemd/user/hermes-dashboard.service
+    chown -R hermes:hermes /home/hermes/.config
+    su - hermes -c 'systemctl --user enable hermes-dashboard'
+    msg_info "Migration Complete"
+  else
+    msg_info "Stopping Services"
+    su - hermes -c 'systemctl --user stop hermes-dashboard'
+    msg_ok "Stopped Services"
+  fi
 
   msg_info "Updating Hermes Agent"
   $STD setsid --wait su - hermes -c '
